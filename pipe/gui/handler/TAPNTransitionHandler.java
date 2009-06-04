0d7b60c4f0dc640cef97ec0c9b05cc34a3d0086d
yrke <yrke>
2009-06-04 14:26:30 +0200

    Fixes shift scroll behaviour from TAPNTranistions (related to lift/right scrolling on mac) bug #363158

--- pipe/gui/handler/TAPNTransitionHandler.java
+++ pipe/gui/handler/TAPNTransitionHandler.java
@@ -5,11 +5,13 @@ import java.awt.MenuComponent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
 
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 
 import pipe.dataLayer.Transition;
+import pipe.gui.CreateGui;
 import pipe.gui.action.ShowHideInfoAction;
 
 public class TAPNTransitionHandler extends TransitionHandler {
@@ -18,6 +20,31 @@ public class TAPNTransitionHandler extends TransitionHandler {
 		super(contentpane, obj);
 		// TODO Auto-generated constructor stub
 	}
+	
+	//overwrite to remove shift behaviour
+	public void mouseWheelMoved (MouseWheelEvent e) {
+
+      if (CreateGui.getApp().isEditionAllowed() == false || 
+              e.isControlDown()) {
+         return;
+      }
+      
+      if (e.isShiftDown()) {
+  /*       CreateGui.getView().getUndoManager().addNewEdit(
+                 ((Transition)myObject).setTimed(
+                 !((Transition)myObject).isTimed()));*/
+      } else {
+         int rotation = 0;
+         if (e.getWheelRotation() < 0) {
+            rotation = -e.getWheelRotation() * 135;
+         } else {
+            rotation = e.getWheelRotation() * 45;
+         }
+         CreateGui.getView().getUndoManager().addNewEdit(
+                 ((Transition)myObject).rotate(rotation));
+      }
+   }   
+   
 	/** 
 	    * Creates the popup menu that the user will see when they right click on a 
 	    * component 
