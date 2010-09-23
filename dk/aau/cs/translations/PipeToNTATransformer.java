e5ed96f829f8e4f2865a3354a924e6116dbaaa08
Morten Jacobsen <mortenja@ubuntu>
2010-09-23 18:35:08 +0200

    made PipeToNTATransformer aware of colors

--- dk/aau/cs/translations/PipeToNTATransformer.java
+++ dk/aau/cs/translations/PipeToNTATransformer.java
@@ -3,19 +3,27 @@ package dk.aau.cs.translations;
 import pipe.dataLayer.DataLayer;
 import dk.aau.cs.TA.NTA;
 import dk.aau.cs.TAPN.ModelTransformer;
+import dk.aau.cs.petrinet.PipeTapnToAauTapnTransformer;
 import dk.aau.cs.petrinet.TimedArcPetriNet;
+import dk.aau.cs.petrinet.colors.ColoredPipeTapnToColoredAauTapnTransformer;
 
 public class PipeToNTATransformer implements ModelTransformer<DataLayer, NTA> {
 
-	private ModelTransformer<DataLayer, TimedArcPetriNet> pipeToTAPNtransformer;
 	private ModelTransformer<TimedArcPetriNet, NTA> tapnToNtaTransformer;
 	
 	public PipeToNTATransformer(ModelTransformer<TimedArcPetriNet, NTA> tapnToNtaTransformer){
-		this.tapnToNtaTransformer = tapnToNtaTransformer;
+		this.tapnToNtaTransformer = tapnToNtaTransformer; // TODO: MJ -- This has to be fixed at some point, you need to specify a colored version if it requires colors
 	}
 	
 	public NTA transformModel(DataLayer model) throws Exception {
-		TimedArcPetriNet tapn = pipeToTAPNtransformer.transformModel(model);
+		PipeTapnToAauTapnTransformer pipeTransformer;
+		if(model.isUsingColors()){
+			pipeTransformer = new ColoredPipeTapnToColoredAauTapnTransformer();
+		}else{
+			pipeTransformer = new PipeTapnToAauTapnTransformer();
+		}
+		
+		TimedArcPetriNet tapn = pipeTransformer.transformModel(model);
 		return tapnToNtaTransformer.transformModel(tapn);
 	}
 }
