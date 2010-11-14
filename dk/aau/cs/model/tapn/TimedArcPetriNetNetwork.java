115424146a002af719b96b8fc39fd8a7feff45a4
Morten Jacobsen <morten.jacobsen.2k@gmail.com>
2010-11-14 13:12:13 +0100

    added network of timed-arc petri nets. This will contain the templates.

---
+++ dk/aau/cs/model/tapn/TimedArcPetriNetNetwork.java
@@ -0,0 +1,30 @@
+package dk.aau.cs.model.tapn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dk.aau.cs.util.Require;
+
+public class TimedArcPetriNetNetwork {
+	private List<TimedArcPetriNet> tapns;
+	
+	public TimedArcPetriNetNetwork(){
+		tapns = new ArrayList<TimedArcPetriNet>();
+	}
+	
+	public void add(TimedArcPetriNet tapn){
+		Require.that(tapn != null, "tapn must be non-null");
+		
+		tapns.add(tapn);
+	}
+	
+	public void remove(TimedArcPetriNet tapn){
+		if(tapn != null){
+			tapns.remove(tapn);
+		}
+	}
+	
+	public List<TimedArcPetriNet> templates(){
+		return tapns;
+	}
+}
