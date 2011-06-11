db4f213963f186b2f4d59e10da0502ca38f3eb7a
Morten Jacobsen <morten.jacobsen.2k@gmail.com>
2011-06-11 16:03:45 +0200

    Added info icon to the repository (still not used in the tool).

---
+++ src/dk/aau/cs/io/ResourceManager.java
@@ -0,0 +1,43 @@
+package dk.aau.cs.io;
+
+import java.awt.Image;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+import pipe.gui.CreateGui;
+
+public class ResourceManager {
+	private static ImageIcon satisfiedIcon = loadIcon("satisfied.png");
+	private static ImageIcon notSatisfiedIcon = loadIcon("notsatisfied.png");
+	private static ImageIcon inconclusiveIcon = loadIcon("maybe1.png");
+	private static ImageIcon infoIcon = loadIcon("info.png");
+	
+	private static ImageIcon loadIcon(String name) {
+		try {
+			return new ImageIcon(ImageIO.read(Thread.currentThread().getContextClassLoader().getResource(CreateGui.imgPath + name)).getScaledInstance(52, 52, Image.SCALE_SMOOTH));
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch(IllegalArgumentException e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static ImageIcon satisfiedIcon(){
+		return satisfiedIcon;
+	}
+	
+	public static ImageIcon notSatisfiedIcon(){
+		return notSatisfiedIcon;
+	}
+	
+	public static ImageIcon inconclusiveIcon(){
+		return inconclusiveIcon;
+	}
+	
+	public static ImageIcon infoIcon(){
+		return infoIcon;
+	}
+}
--- src/dk/aau/cs/verification/IconSelector.java
+++ src/dk/aau/cs/verification/IconSelector.java
@@ -1,27 +1,13 @@
 package dk.aau.cs.verification;
 
-import java.awt.Image;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
-
-import pipe.gui.CreateGui;
+import dk.aau.cs.io.ResourceManager;
 
 public abstract class IconSelector {
-	protected static ImageIcon satisfiedIcon = loadIcon("satisfied");
-	protected static ImageIcon notSatisfiedIcon = loadIcon("notsatisfied");
-	protected static ImageIcon inconclusiveIcon = loadIcon("maybe");
-
-	private static ImageIcon loadIcon(String name) {
-		try {
-			return new ImageIcon(ImageIO.read(Thread.currentThread().getContextClassLoader().getResource(CreateGui.imgPath + name + ".png")).getScaledInstance(52, 52, Image.SCALE_SMOOTH));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+	protected static ImageIcon satisfiedIcon = ResourceManager.satisfiedIcon();
+	protected static ImageIcon notSatisfiedIcon = ResourceManager.notSatisfiedIcon();
+	protected static ImageIcon inconclusiveIcon = ResourceManager.inconclusiveIcon();
 
 	public abstract ImageIcon getIconFor(QueryResult result);
 }
