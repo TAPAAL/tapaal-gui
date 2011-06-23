1b365bbef3875ef64469bd733f04e71442068bf7
Jiri Srba <srba@cs.aau.dk>
2011-06-23 13:37:48 +0200

    increased the version to 2.0.0

--- src/pipe/gui/Pipe.java
+++ src/pipe/gui/Pipe.java
@@ -5,7 +5,7 @@ import java.awt.Color;
 public class Pipe {
    
    public static final String TOOL = "TAPAAL";
-   public static final String VERSION = "DEV"; 
+   public static final String VERSION = "2.0.0"; 
    
    public static String getProgramName(){
 	   return "" + Pipe.TOOL + " " + Pipe.VERSION;
