23197c3c8660af26d728827dc15d682cd148be31
Morten Jacobsen <morten.jacobsen.2k@gmail.com>
2011-09-18 18:46:07 +0200

    forgot to add file

---
+++ src/dk/aau/cs/io/IdResolver.java
@@ -0,0 +1,24 @@
+package dk.aau.cs.io;
+
+import java.util.HashMap;
+
+public class IdResolver {
+	private HashMap<String, HashMap<String, String>> idToNamesPerTemplate = new HashMap<String, HashMap<String, String>>();
+	
+	public void add(String template, String id, String name){
+		if(!idToNamesPerTemplate.containsKey(template)){
+			idToNamesPerTemplate.put(template, new HashMap<String, String>());
+		}
+		
+		HashMap<String, String> idToNames = idToNamesPerTemplate.get(template);
+		idToNames.put(id, name);
+	}
+	
+	public String get(String template, String id){
+		return idToNamesPerTemplate.get(template).get(id);
+	}
+
+	public void clear() {
+		idToNamesPerTemplate.clear();
+	}
+}
