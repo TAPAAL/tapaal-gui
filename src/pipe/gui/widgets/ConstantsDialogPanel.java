e9c8e392b3a6c4a19e7aa1d059ec3d0630981e43
Mathias Andersen <mande09@student.aau.dk>
2011-10-31 21:11:29 +0100

    changes to ConstantsDialogPanel mainly

--- src/pipe/gui/widgets/ConstantsDialogPanel.java
+++ src/pipe/gui/widgets/ConstantsDialogPanel.java
@@ -6,6 +6,7 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.NumberFormat;
 import java.util.regex.Pattern;
 
 import javax.swing.BoxLayout;
@@ -20,7 +21,17 @@ import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.event.AncestorEvent;
 import javax.swing.event.AncestorListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.AbstractDocument;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultEditorKit;
+import javax.swing.text.DefaultFormatterFactory;
+import javax.swing.text.DocumentFilter;
+import javax.swing.text.InternationalFormatter;
 import javax.swing.text.NumberFormatter;
+import javax.swing.text.DocumentFilter.FilterBypass;
 
 import pipe.gui.CreateGui;
 import dk.aau.cs.gui.undo.Command;
@@ -57,6 +68,8 @@ public class ConstantsDialogPanel extends javax.swing.JPanel {
 	JPanel container;
 	
 	private String oldName;
+	//private String currentTextValueOfSpinner;
+	//private boolean spinnerTextIsEmptyString;
 
 	/** Creates new form LeftConstantsPane */
 
@@ -72,10 +85,13 @@ public class ConstantsDialogPanel extends javax.swing.JPanel {
 		this.model = model;
 
 		oldName = "";
-
+		//currentTextValueOfSpinner = "0";
+		//spinnerTextIsEmptyString = false;
+		
 		// Set up initial values
-		SpinnerNumberModel spinnerModel = new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1);
-		valueSpinner.setModel(spinnerModel);
+		//SpinnerNumberModel spinnerModel = new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1);
+		//valueSpinner.setModel(spinnerModel);
+		//((JSpinner.NumberEditor) valueSpinner.getEditor()).getTextField().setText("");
 		nameTextField.setText(oldName);
 	}
 
@@ -87,10 +103,15 @@ public class ConstantsDialogPanel extends javax.swing.JPanel {
 		lowerBound = constant.lowerBound();
 		upperBound = constant.upperBound();
 
-		SpinnerNumberModel spinnerModel = new SpinnerNumberModel(constant
-				.value(), 0, constant.upperBound(), 1);
-		valueSpinner.setModel(spinnerModel);
+		//SpinnerNumberModel spinnerModel = new SpinnerNumberModel(constant
+			//	.value(), 0, constant.upperBound(), 1);
+		//valueSpinner.setModel(spinnerModel);
+		//((JSpinner.NumberEditor) valueSpinner.getEditor()).getTextField().setText("");
+		//((JSpinner.NumberEditor) valueSpinner.getEditor()).set
+		 
 		nameTextField.setText(oldName);
+		//currentTextValueOfSpinner = "0";
+		//spinnerTextIsEmptyString = false;
 	}
 	
 	public void showDialog() {
@@ -115,6 +136,7 @@ public class ConstantsDialogPanel extends javax.swing.JPanel {
 						CreateGui.getApp(),
 						"Acceptable names for constants are defined by the regular expression:\n[a-zA-Z][_a-zA-Z0-9]*",
 						"Error", JOptionPane.ERROR_MESSAGE);
+				showDialog();
 				return;
 			}
 
@@ -122,8 +144,18 @@ public class ConstantsDialogPanel extends javax.swing.JPanel {
 				JOptionPane.showMessageDialog(CreateGui.getApp(),
 						"You must specify a name.", "Missing name",
 						JOptionPane.ERROR_MESSAGE);
-				return;
+				showDialog();
+				return;				
 			} else {
+				//check for empty string in value field
+				//if show error message; showDialog(); and return;
+//				if (currentTextValueOfSpinner.isEmpty()){
+//					JOptionPane.showMessageDialog(CreateGui.getApp(),
+//							"You must specify a value.", "Missing value",
+//							JOptionPane.ERROR_MESSAGE);
+//					showDialog();
+//					return;
+//				}
 				int val = (Integer) valueSpinner.getValue();
 
 				if (!oldName.equals("")) {
@@ -135,6 +167,7 @@ public class ConstantsDialogPanel extends javax.swing.JPanel {
 								"There is already another constant with the same name.\n\n"
 								+ "Choose a different name for the constant.",
 								"Error", JOptionPane.ERROR_MESSAGE);
+						showDialog();
 						return;
 					}
 					//Kyrke - This is messy, but a quck fix for bug #815487			
@@ -147,6 +180,7 @@ public class ConstantsDialogPanel extends javax.swing.JPanel {
 								+ "on one or more arcs.",
 								"Constant value invalid for current net",
 								JOptionPane.ERROR_MESSAGE);
+						showDialog();
 						return;
 					}
 					Command edit = model.updateConstant(oldName, new Constant(
@@ -160,6 +194,7 @@ public class ConstantsDialogPanel extends javax.swing.JPanel {
 								+ "on one or more arcs.",
 								"Constant value invalid for current net",
 								JOptionPane.ERROR_MESSAGE);
+						showDialog();
 						return;
 					} else {
 						CreateGui.getCurrentTab().drawingSurface().getUndoManager()
@@ -175,6 +210,7 @@ public class ConstantsDialogPanel extends javax.swing.JPanel {
 								"A constant with the specified name already exists.",
 								"Constant exists",
 								JOptionPane.ERROR_MESSAGE);
+						showDialog();
 						return;
 					} else
 						CreateGui.getView().getUndoManager().addNewEdit(edit);
@@ -188,14 +224,102 @@ public class ConstantsDialogPanel extends javax.swing.JPanel {
 	 * WARNING: Do NOT modify this code. The content of this method is always
 	 * regenerated by the Form Editor.
 	 */
-
+//    class spinnerDocumentFilter extends DocumentFilter {
+//
+//		@Override
+//		public void insertString(FilterBypass fb, int offset, String string,
+//				AttributeSet attr) throws BadLocationException {
+//			// TODO Auto-generated method stub
+//			super.insertString(fb, offset, string, attr);
+//			System.out.println("insert, string: "+ string+", offset: "+offset+
+//					", fb.doc: "+fb.getDocument().getText(0,fb.getDocument().getLength()));
+//		}
+//
+//		@Override
+//		public void remove(FilterBypass fb, int offset, int length)
+//				throws BadLocationException {
+//			// TODO Auto-generated method stub
+//			super.remove(fb, offset, length);
+//			System.out.println("remove, offset: "+offset+"length: "+length);
+//		}
+//
+//		@Override
+//		public void replace(FilterBypass fb, int offset, int length,
+//				String text, AttributeSet attrs) throws BadLocationException {
+//			// TODO Auto-generated method stub
+//			super.replace(fb, offset, length, text, attrs);
+//			System.out.println("replace, string: "+text+", offset: "+offset+", length"+length);
+//		}
+//    	
+//    }
+//	 private JSpinner makeDigitsOnlySpinnerUsingDocumentFilter() {
+//         JSpinner spinner = new JSpinner(new SpinnerNumberModel());
+//         JSpinner.NumberEditor jsEditor =
+//             (JSpinner.NumberEditor)spinner.getEditor();
+//
+//         JFormattedTextField textField = jsEditor.getTextField();
+//         final DocumentFilter digitOnlyFilter = new DocumentFilter() {
+//             @Override
+//             public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
+//                 if (stringContainsOnlyDigits(string)) {
+//                     super.insertString(fb, offset, string, attr);
+//                 }
+//                 System.out.println("test");
+//             }
+//
+//             @Override
+//             public void remove(FilterBypass fb, int offset, int length) throws BadLocationException {
+//                 super.remove(fb, offset, length);
+//                 System.out.println("test");
+//             }
+//
+//             @Override
+//             public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+//                 if (stringContainsOnlyDigits(text)) {
+//                     super.replace(fb, offset, length, text, attrs);
+//                 }
+//                 System.out.println("test");
+//             }
+//
+//             private boolean stringContainsOnlyDigits(String text) {
+//                 for (int i = 0; i<text.length(); i++) {
+//                     if (!Character.isDigit(text.charAt(i))) {
+//                         return false;
+//                     }
+//                 }
+//                 return true;
+//             }
+//         };
+//
+//         NumberFormat format = NumberFormat.getIntegerInstance();
+//         // or add the group chars to the filter
+//         format.setGroupingUsed(false);
+//         textField.setFormatterFactory(new DefaultFormatterFactory(
+//                 new InternationalFormatter(format){
+//             /**
+//					 * 
+//					 */
+//					private static final long serialVersionUID = 1L;
+//
+//			@Override
+//             protected DocumentFilter getDocumentFilter() {
+//                 return digitOnlyFilter;
+//             }
+//         }));
+//
+//         return spinner;
+//     }
 	
 	private void initComponents() {
+		valueSpinner = new JSpinner(new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1));
+		valueSpinner.setEditor(new JSpinner.NumberEditor(valueSpinner));
+		((NumberFormatter)(((JSpinner.DefaultEditor)valueSpinner.getEditor()).getTextField().getFormatter())).setAllowsInvalid(false);
+		
 		container = new JPanel();
 		container.setLayout(new GridBagLayout());
 		size = new Dimension(250, 25);
 		
-		nameTextField = new javax.swing.JTextField();		
+		nameTextField = new javax.swing.JTextField();	
 		nameTextField.setPreferredSize(size);
 		nameTextField.addAncestorListener(new RequestFocusListener());
 
@@ -226,7 +350,7 @@ public class ConstantsDialogPanel extends javax.swing.JPanel {
 		gbc.anchor = GridBagConstraints.WEST;
 		container.add(valueLabel,gbc);
 		
-		valueSpinner = new javax.swing.JSpinner();
+		//valueSpinner = new javax.swing.JSpinner();
 		gbc = new GridBagConstraints();
 		gbc.insets = new Insets(2, 0, 2, 0);
 		gbc.gridx = 1;
@@ -235,6 +359,75 @@ public class ConstantsDialogPanel extends javax.swing.JPanel {
 		gbc.anchor = GridBagConstraints.WEST;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		container.add(valueSpinner,gbc);
+		
+//		valueSpinner =  makeDigitsOnlySpinnerUsingDocumentFilter();
+		
+		//((JTextField)((JSpinner.DefaultEditor)valueSpinner.getEditor()).getTextField()).setEditable(false);
+		//action listeners:
+		//up and down button
+		//enter text to value box?
+		//JTextField txt = new JTextField(); 
+		//valueSpinner.setEditor(txt);
+		//((AbstractDocument)txt.getDocument()).setDocumentFilter(new spinnerDocumentFilter());
+		//((AbstractDocument)nameTextField.getDocument()).setDocumentFilter(new spinnerDocumentFilter());
+		//((AbstractDocument)((JTextField)((JSpinner.DefaultEditor)valueSpinner.getEditor()).getTextField()).getDocument()).setDocumentFilter(new spinnerDocumentFilter());
+		//((JTextField)((JSpinner.DefaultEditor)valueSpinner.getEditor()).getTextField()).getDocument().addDocumentListener(new DocumentListener() {
+			  //public void changedUpdate(DocumentEvent e) {
+			    //warn();
+				//  System.out.println("changed..");				  
+			  //}
+			  //public void removeUpdate(DocumentEvent e) {
+//			    //warn();
+//				  //System.out.println("removeupdated..");
+//				  try {
+////					 System.out.println("value is: "+
+////					e.getDocument().getText(0, e.getDocument().getLength())+".");
+//					 if (!Pattern.matches("([1-9]([0-9])*)|([0])", e.getDocument().getText(0, e.getDocument().getLength()))
+//							 && !e.getDocument().getText(0, e.getDocument().getLength()).isEmpty()) {
+//						  //accepted = false;
+//						((JSpinner.NumberEditor) valueSpinner.getEditor()).getTextField().setText(currentTextValueOfSpinner);
+//					  } else {
+//						currentTextValueOfSpinner = e.getDocument().getText(0, e.getDocument().getLength());
+//					  }
+//				} catch (BadLocationException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
+			  //}
+			  //public void insertUpdate(DocumentEvent e) {
+//			    //warn();
+//				  //System.out.println("insertUpdated..");
+//				  //System.out.println(valueSpinner.isShowing());
+//				  try {
+//					  //System.out.println("value is: "+
+//					//e.getDocument().getText(0, e.getDocument().getLength())+".");
+//					  //boolean accepted = true;
+//					  if (e.getDocument().getText(0, e.getDocument().getLength()) == "0") {
+//						  if (!valueSpinner.isShowing())
+//							  if (currentTextValueOfSpinner != "0")
+//								  return;
+//					  }
+//					  if (!Pattern.matches("([1-9]([0-9])*)|([0])", e.getDocument().getText(0, e.getDocument().getLength()))) {
+//						  //accepted = false;
+//						((JSpinner.NumberEditor) valueSpinner.getEditor()).getTextField().setText(currentTextValueOfSpinner);
+//					  } else {
+//						currentTextValueOfSpinner = e.getDocument().getText(0, e.getDocument().getLength());
+//					  }
+//					  //System.out.println("accepted: "+accepted);
+//				} catch (BadLocationException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//				}
+			  //}
+
+//			  public void warn() {
+//			     if (Integer.parseInt(textField.getText())<=0){
+//			       JOptionPane.showMessageDialog(null,
+//			          "Error: Please enter number bigger than 0", "Error Massage",
+//			          JOptionPane.ERROR_MESSAGE);
+//			     }
+//			  }
+			//});
 	}
 }
 
--- src/pipe/gui/widgets/RequestFocusListener.java
+++ src/pipe/gui/widgets/RequestFocusListener.java
@@ -9,38 +9,13 @@ public class RequestFocusListener implements AncestorListener
 {
 	private boolean removeListener;
 
-	/*
-	 *  Convenience constructor. The listener is only used once and then it is
-	 *  removed from the component.
-	 */
 	public RequestFocusListener()
 	{
-		this(true);
-	}
-
-	/*
-	 *  Constructor that controls whether this listen can be used once or
-	 *  multiple times.
-	 *
-	 *  @param removeListener when true this listener is only invoked once
-	 *                        otherwise it can be invoked multiple times.
-	 */
-	public RequestFocusListener(boolean removeListener)
-	{
-		this.removeListener = removeListener;
 	}
 
 	@Override
 	public void ancestorAdded(final AncestorEvent arg0)
 	{
-//		JComponent component = arg0.getComponent();
-//		component.requestFocusInWindow();
-//
-//		if (removeListener)
-//			component.removeAncestorListener( this );
-		
-		//the above commented out code does not work in linux for some reason. 
-		//The code below is a hack to get around it.
 		 final AncestorListener al= this;   
 		    SwingUtilities.invokeLater(new Runnable(){
 
