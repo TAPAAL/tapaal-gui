fd52c1055825a8ed4bb4fe5094ddb6f77105773f
Jakob Taankvist <jakob_taankvist@hotmail.com>
2011-11-16 10:10:50 +0100

    Added buttons for removing orphans and checking for boundedness, and implemented the action to remove orphans (though shared transitions causes some problems, so for now they are not deleted)

--- src/dk/aau/cs/gui/components/StatisticsPanel.java
+++ src/dk/aau/cs/gui/components/StatisticsPanel.java
@@ -1,20 +1,37 @@
 package dk.aau.cs.gui.components;
 
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 
+import pipe.gui.*;
+import pipe.gui.graphicElements.tapn.TimedTransitionComponent;
+import pipe.gui.undo.DeleteTimedTransitionCommand;
+import pipe.gui.undo.UndoManager;
+import pipe.dataLayer.*;
+
+import dk.aau.cs.gui.TabContent;
+import dk.aau.cs.model.tapn.TimedArcPetriNet;
+import dk.aau.cs.model.tapn.TimedTransition;
 import dk.aau.cs.util.Require;
 
 public class StatisticsPanel extends JPanel{
 	private final int topBottomMargin = 3;
 	private final int rightMargin = 10;
+	
+	private JButton removeOrphans;
+	private JButton checkBoundedness;
 	
 	public StatisticsPanel(Object[][] contents, Object[] headLines) {
 		super(new GridBagLayout());
@@ -36,12 +53,16 @@ public class StatisticsPanel extends JPanel{
 		gbc.gridwidth = headLines.length;
 		JSeparator jSep;
 		gbc.insets = new Insets(topBottomMargin, 0, topBottomMargin, rightMargin);
-		for(int i = 3; i<contents.length*2; i += 2){
+		for(int i = 3; i<contents.length*2+2; i += 2){
 			jSep = new JSeparator();
 			jSep.setPreferredSize(new Dimension(1, 3));
 			gbc.gridy = i;
 			this.add(jSep, gbc);
 		}
+		
+		//Add buttons
+		addButtons(headLines.length, contents.length*2);
+		
 	}
 	
 	private void addRow(Object[] row, int rowNumber, boolean isHeadLine){
@@ -64,5 +85,46 @@ public class StatisticsPanel extends JPanel{
 				}
 			}
 		}
+	}
+	
+	private void addButtons(int gridWidth, int gridHeight){
+		removeOrphans = new JButton("Remove orphan transitions");
+		removeOrphans.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				TabContent tab = CreateGui.getCurrentTab();
+				Iterable<Template> templates = tab.allTemplates();
+				UndoManager undoManager = CreateGui.getDrawingSurface().getUndoManager();
+				for(Template template : templates){
+					List<TimedTransition> orphans = template.model().getOrphanTransitions();
+					for(TimedTransition trans : orphans){
+						//Shared transistions makes truble
+						if(!trans.isShared()){
+							TimedTransitionComponent t = (TimedTransitionComponent)template.guiModel().getTransitionByName(trans.name());
+							undoManager.addEdit(new DeleteTimedTransitionCommand(t, t.underlyingTransition().model(), template.guiModel(), tab.drawingSurface()));
+							t.delete();
+						}
+					}
+				}
+				
+				tab.drawingSurface().repaint();
+			}
+		});
+		
+		checkBoundedness = new JButton("Check boundedness");
+		
+		JPanel buttonsPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		buttonsPanel.add(removeOrphans);
+		
+		buttonsPanel.add(checkBoundedness);
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = gridHeight + 2;
+		gbc.anchor = GridBagConstraints.EAST;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridwidth = gridWidth;
+		this.add(buttonsPanel, gbc);
 	}
 }
