211aafd8b338ea1c1c9db6c69d8c06923fe9e11e
Jiri Srba <srba@cs.aau.dk>
2012-02-17 21:48:40 +0100

    updated About

--- src/pipe/gui/GuiFrame.java
+++ src/pipe/gui/GuiFrame.java
@@ -1866,19 +1866,19 @@ public class GuiFrame extends JFrame implements Observer {
 	public void showAbout() {
 		StringBuffer buffer = new StringBuffer("About " + TAPAAL.TOOLNAME);
 		buffer.append("\n\n");
+		buffer.append("TAPAAL is a tool for editing, simulation and verification of timed-arc Petri nets.\n");
+		buffer.append("The GUI is based on PIPE2: http://pipe2.sourceforge.net/\n\n");
+		buffer.append("License information and more is avaialbe at: www.tapaal.net\n\n");
 		buffer.append("Credits\n\n");
 		buffer.append("TAPAAL GUI and Translations:\n");
 		buffer.append("Mathias Andersen, Joakim Byg, Lasse Jacobsen, Morten Jacobsen,\n");
 		buffer.append("Kenneth Yrke Joergensen, Mikael H. Moeller, Jiri Srba and Jakob H. Taankvist\n");
-		buffer.append("Aalborg University 2009-2011\n\n");
+		buffer.append("Aalborg University 2009-2012\n\n");
 		buffer.append("TAPAAL Engine:\n");
 		buffer.append("Alexandre David, Lasse Jacobsen, Morten Jacobsen and Jiri Srba\n");
-		buffer.append("Aalborg University 2011\n\n");
-		buffer.append("License information and more at: www.tapaal.net\n\n");
-
-		buffer.append("  \n\n");
-		buffer.append("Based on PIPE2:\n");
-		buffer.append("http://pipe2.sourceforge.net/");
+		buffer.append("Aalborg University 2011-2012\n\n");
+		buffer.append("\n");
+		
 
 		JOptionPane.showMessageDialog(null, buffer.toString(), "About " + TAPAAL.TOOLNAME,
 				JOptionPane.INFORMATION_MESSAGE, ResourceManager.appIcon());
