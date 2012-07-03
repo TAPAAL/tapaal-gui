4e10605b230bec5b75343c2500592d45035b44a3
jakob_taankvist <jakob_taankvist@hotmail.com>
2012-07-03 14:44:38 +0200

    The batch processing should now work without throwing exceptions

--- src/dk/aau/cs/verification/UPPAAL/Verifyta.java
+++ src/dk/aau/cs/verification/UPPAAL/Verifyta.java
@@ -258,7 +258,7 @@ public class Verifyta implements ModelChecker {
 				return new VerificationResult<TimedArcPetriNetTrace>(errorOutput + System.getProperty("line.separator") + standardOutput, runner.getRunningTime());
 			} else {
 				TimedArcPetriNetTrace tapnTrace = parseTrace(errorOutput, options, model, exportedModel, query, queryResult);
-				return new VerificationResult<TimedArcPetriNetTrace>(queryResult, tapnTrace, runner.getRunningTime(), null);
+				return new VerificationResult<TimedArcPetriNetTrace>(queryResult, tapnTrace, runner.getRunningTime());
 			}
 		}
 	}
