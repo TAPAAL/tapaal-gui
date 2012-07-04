6130129d79da1236913796d413cb85b28d1978d2
Mathias Grund Sørensen <mgso09@student.aau.dk>
2012-07-04 13:33:52 +0200

    MERGE with 2.1 corrections for Bug #965303 (3/3)

--- src/dk/aau/cs/verification/batchProcessing/BatchProcessingWorker.java
+++ src/dk/aau/cs/verification/batchProcessing/BatchProcessingWorker.java
@@ -147,7 +147,6 @@ public class BatchProcessingWorker extends SwingWorker<Void, BatchProcessingVeri
 	}
 
 	private void processQuery(File file, Tuple<TimedArcPetriNet, NameMapping> composedModel, pipe.dataLayer.TAPNQuery queryToVerify) throws Exception {
-		fireVerificationTaskStarted();
 		if(queryToVerify.isActive()) { 
 			VerificationResult<TimedArcPetriNetTrace> verificationResult = verifyQuery(file, composedModel, queryToVerify);
 			
@@ -251,7 +250,7 @@ public class BatchProcessingWorker extends SwingWorker<Void, BatchProcessingVeri
 		
 		VerificationOptions options = getVerificationOptionsFromQuery(query);
 		modelChecker = getModelChecker(query);
-		
+		fireVerificationTaskStarted();
 		VerificationResult<TimedArcPetriNetTrace> verificationResult = modelChecker.verify(options, composedModel, queryToVerify);
 		return verificationResult;
 	}
