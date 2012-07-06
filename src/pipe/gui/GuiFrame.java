e750fb2865c618886e02a9affd0f1509c36c9920
Mathias Grund Sørensen <mgso09@student.aau.dk>
2012-07-06 13:49:54 +0200

    Removed fix for bug #812694 as it is no longer necessary

--- src/pipe/gui/GuiFrame.java
+++ src/pipe/gui/GuiFrame.java
@@ -1665,14 +1665,6 @@ public class GuiFrame extends JFrame implements Observer {
 				stepforwardAction.setEnabled(false);
 				stepbackwardAction.setEnabled(false);
 					
-				// XXX
-				// This is a fix for bug #812694 where on mac some menues are gray after
-				// changing from simulation mode, when displaying a trace. Showing and 
-				// hiding a menu seems to fix this problem 
-				Dialog a = new Dialog(CreateGui.appGui);
-				a.setVisible(true);
-				a.setVisible(false);
-				a.dispose();
 				if(getGUIMode().equals(GUIMode.draw)){
 					activateSelectAction();
 				}				
