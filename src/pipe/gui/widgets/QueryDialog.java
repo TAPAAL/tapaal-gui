cf04ade8a684555ba98b6303a9cd968ec05ca964
jakob_taankvist <jakob_taankvist@hotmail.com>
2012-07-09 12:31:31 +0200

    Symmetry reduction is now allways selected with the discrete engine

--- src/pipe/gui/widgets/QueryDialog.java
+++ src/pipe/gui/widgets/QueryDialog.java
@@ -2033,14 +2033,14 @@ public class QueryDialog extends JPanel {
 	}
 
 	private void refreshSymmetryReduction() {
-//		if(((String)reductionOption.getSelectedItem()).equals(name_verifyTAPN)) {
-//			//symmetryReduction.setSelected(true);
-//			//symmetryReduction.setEnabled(false);
-//		}
-//		else{
-//			symmetryReduction.setSelected(symmetryReduction.isSelected());
-//			symmetryReduction.setEnabled(true);
-//		}
+		if(((String)reductionOption.getSelectedItem()).equals(name_DISCRETE)) {
+			symmetryReduction.setSelected(true);
+			symmetryReduction.setEnabled(false);
+		}
+		else{
+			symmetryReduction.setSelected(symmetryReduction.isSelected());
+			symmetryReduction.setEnabled(true);
+		}
 	}
 	
 //	private void queryChanged(){
