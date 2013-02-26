672ce92af54e81003829897808eecc7a5cdaa625
Mathias Grund Sørensen <mgso09@student.aau.dk>
2013-02-26 11:17:48 +0100

    Memory reported on Windows. Issue with time scew.

--- src/dk/aau/cs/util/MemoryMonitor.java
+++ src/dk/aau/cs/util/MemoryMonitor.java
@@ -11,6 +11,11 @@ import java.util.concurrent.Semaphore;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
+import pipe.gui.CreateGui;
+
 public class MemoryMonitor {
 
 	static interface Kernel32 extends Library {
@@ -22,63 +27,64 @@ public class MemoryMonitor {
 
 	private static int PID = -1;
 	private static Semaphore busy = new Semaphore(1);
-	
+
 	public static void attach(Process p){
 		PID = getPid(p);
 		System.out.println("Process "+PID+" started.");
 	}
-	
+
 	public static void detach(Process p){
 		if(PID == getPid(p)){
 			PID = -1;
 			System.out.println("MemoryMonitor detached.");
 		}
 	}
-	
+
 	public static boolean isAttached(){
 		return PID != -1;
 	}
-	
+
 	public static String getUsage(){
 		if(busy.tryAcquire()){
 			double memory = -1;
 			DecimalFormat formatter = new DecimalFormat("#,##0.0");
 			if(Platform.isWindows()){
 				try { 
-	                Process p = Runtime.getRuntime().exec("tasklist /FI \"pid eq "+PID+"\" /FO \"LIST\""); 
-	                BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream())); 
-	               
-	                StringBuilder s = new StringBuilder();
-	                String tmp = input.readLine();
-	                while(tmp != null){
-	                	s.append(tmp.trim());
-	                	tmp = input.readLine();
-	                }
-	                
-	                Pattern pattern = Pattern.compile(".*?([.,0-9]*) K.*");
-	                Matcher m = pattern.matcher(s.toString());
-	                memory = Double.parseDouble(m.group(1))/1024;
+					Process p = Runtime.getRuntime().exec("tasklist /FI \"pid eq "+PID+"\" /FO \"LIST\""); 
+					BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream())); 
+
+					StringBuilder s = new StringBuilder();
+					String tmp = input.readLine();
+					while(tmp != null){
+						s.append(tmp.trim());
+						tmp = input.readLine();
+					}
+
+					Pattern pattern = Pattern.compile(".*?([.,0-9]*) K.*");
+					Matcher m = pattern.matcher(s.toString());
+					if(m.matches()){
+						memory = Double.parseDouble(m.group(1).replace(".", "").replace(",", ""))/1024;
+					}
 				} catch (Exception e) { 
-	           	  System.err.println("Error reading memory: "+e.getMessage());
-	           	  return e.getMessage();
-	             } 
+					System.err.println("Error reading memory: "+e.getMessage());
+				} 
 			}else{
-				 try { 
-	                 Process p = Runtime.getRuntime().exec("ps -p "+PID+" -o rss"); 
-	                 BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream())); 
-	                 //Actual memory usage is second line output
-	                 String s = input.readLine();
-	                 s = input.readLine();
-	                 memory = Double.parseDouble(s.replace(" ", ""))/1024;
-	              } catch (Exception e) { 
-	            	  System.err.println("Error reading memory: "+e.getMessage());
-	              } 
+				try { 
+					Process p = Runtime.getRuntime().exec("ps -p "+PID+" -o rss"); 
+					BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream())); 
+					//Actual memory usage is second line output
+					String s = input.readLine();
+					s = input.readLine();
+					memory = Double.parseDouble(s.replace(" ", ""))/1024;
+				} catch (Exception e) { 
+					System.err.println("Error reading memory: "+e.getMessage());
+				} 
 			}
-			
+
 			busy.release();
-			
+
 			if(memory < 0){
-				return "N/A";
+				return null;
 			}else if(memory >= 1024){
 				return formatter.format(memory/1024) + " GB";
 			}else{
