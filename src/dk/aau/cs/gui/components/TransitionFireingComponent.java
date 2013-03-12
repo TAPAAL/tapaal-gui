9f8511e78afea6fed9187032ff2eeb41e75b0b74
Mathias Grund Sørensen <mgso09@student.aau.dk>
2013-03-12 09:15:32 +0100

    Delay & Fire now responds to enter after click (can be hold)

--- src/dk/aau/cs/gui/components/TransitionFireingComponent.java
+++ src/dk/aau/cs/gui/components/TransitionFireingComponent.java
@@ -6,6 +6,11 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -51,6 +56,15 @@ public class TransitionFireingComponent extends JPanel {
 			public void actionPerformed(ActionEvent e) {
 				enabledTransitionsList.fireSelectedTransition();
 				CreateGui.getApp().setRandomAnimationMode(false);
+			}
+		});
+		fireButton.addKeyListener(new KeyAdapter() {			
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_ENTER){
+					enabledTransitionsList.fireSelectedTransition();
+					CreateGui.getApp().setRandomAnimationMode(false);
+				}
 			}
 		});
 		
