007dce38bfa30f10ce07927f2652fc07e3b8862e
Peter Gjoel Jensen <peter.gjoel@gmail.com>
2013-07-15 23:00:16 +0200

    added checks to batch processing

--- src/dk/aau/cs/verification/VerifyTAPN/VerifyTAPNDiscreteVerification.java
+++ src/dk/aau/cs/verification/VerifyTAPN/VerifyTAPNDiscreteVerification.java
@@ -25,6 +25,7 @@ import dk.aau.cs.TCTL.TCTLAFNode;
 import dk.aau.cs.TCTL.TCTLAGNode;
 import dk.aau.cs.TCTL.TCTLEFNode;
 import dk.aau.cs.TCTL.TCTLEGNode;
+import dk.aau.cs.TCTL.visitors.HasDeadlockVisitor;
 import dk.aau.cs.model.tapn.LocalTimedPlace;
 import dk.aau.cs.model.tapn.TAPNQuery;
 import dk.aau.cs.model.tapn.TimedArcPetriNet;
@@ -262,6 +263,9 @@ public class VerifyTAPNDiscreteVerification implements ModelChecker{
 			if(!supportsModel(model.value1()))
 				throw new UnsupportedModelException("Verifydtapn does not support the given model.");
 			
+                        if(!supportsQuery(model.value1(), query, options)){
+                            throw new UnsupportedQueryException("Verifydtapn does not support the given query-option combination. ");
+                        }
 			//if(!supportsQuery(model.value1(), query, options))
 				//throw new UnsupportedQueryException("Verifydtapn does not support the given query.");
 			
@@ -385,9 +389,12 @@ public class VerifyTAPNDiscreteVerification implements ModelChecker{
 		}
 		
 		boolean supportsQuery(TimedArcPetriNet model, TAPNQuery query, VerificationOptions options) {
-			if(query.getProperty() instanceof TCTLEGNode || query.getProperty() instanceof TCTLAFNode) {
-				return false;
-			}
+                        // if liveness, has deadlock proposition and uses timedarts, it is not supported
+			if((query.getProperty() instanceof TCTLEGNode || query.getProperty() instanceof TCTLAFNode)
+                                && new HasDeadlockVisitor().hasDeadLock(query.getProperty()) 
+                                && ((VerifyDTAPNOptions)options).timeDarts()){
+                            return false;
+                        }
 			
 			return true;
 		}
