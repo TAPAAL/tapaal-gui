4c2f532be8a55e68ffd5c02a1ff03cc116f41915
Jakob Taankvist <jakob_taankvist@hotmail.com>
2013-11-04 18:28:19 +0100

    disabled debug output

--- src/dk/aau/cs/debug/Logger.java
+++ src/dk/aau/cs/debug/Logger.java
@@ -7,7 +7,7 @@ import java.io.PrintStream;
 public class Logger {
 
 	static PrintStream logDevice = System.out;
-	static boolean enableDebug = true;
+	static boolean enableDebug = false;
 
 	public static void changeLogDevice(PrintStream l) {
 		logDevice = l;
