0fb923937438469228f6a1956e6cbc3d78169fab
Jakob Taankvist <jakob_taankvist@hotmail.com>
2014-10-29 20:37:51 +0100

    Fixed the parser of the UPPAAL verification result to work with the new UPPAAL output. The old UPPAAL output was: "Property is satisfied" The new is: "-- Formula is satisfied"
    ------------- This line and the following will be ignored --------------
    
    modified:
      src/dk/aau/cs/verification/UPPAAL/VerifytaOutputParser.java
    unknown:
      classes/
      release-version/

--- src/dk/aau/cs/verification/UPPAAL/VerifytaOutputParser.java
+++ src/dk/aau/cs/verification/UPPAAL/VerifytaOutputParser.java
@@ -8,6 +8,8 @@ import dk.aau.cs.verification.QueryType;
 public class VerifytaOutputParser {
 	private static final String PROPERTY_IS_NOT_SATISFIED_STRING = "Property is NOT satisfied";
 	private static final String PROPERTY_IS_SATISFIED_STRING = "Property is satisfied";
+	private static final String PROPERTY_IS_NOT_SATISFIED_STRING_NEW = "-- Formula is NOT satisfied";
+	private static final String PROPERTY_IS_SATISFIED_STRING_NEW = "-- Formula is satisfied";
 	private static final String DISCRETE_INCLUSION = "discrete inclusion";
 	private boolean error = false;
 	private boolean discreteInclusion = false;
@@ -27,9 +29,9 @@ public class VerifytaOutputParser {
 			for (int i = 0; i < lines.length; i++) {
 				String line = lines[i];
 				if (line.contains(DISCRETE_INCLUSION)) { discreteInclusion = true;}
-				if (line.contains(PROPERTY_IS_SATISFIED_STRING)) {
+				if (line.contains(PROPERTY_IS_SATISFIED_STRING) || line.contains(PROPERTY_IS_SATISFIED_STRING_NEW)) {
 					return new QueryResult(true, new InconclusiveBoundednessAnalysisResult(), query, discreteInclusion);
-				} else if (line.contains(PROPERTY_IS_NOT_SATISFIED_STRING)) {
+				} else if (line.contains(PROPERTY_IS_NOT_SATISFIED_STRING) || line.contains(PROPERTY_IS_NOT_SATISFIED_STRING_NEW)) {
 					return new QueryResult(false, new InconclusiveBoundednessAnalysisResult(), query, discreteInclusion);
 				}
 			}
