a61d40fb6494314546c4a2a34daf0a1a16d1b042
Jiri Srba <srba@cs.aau.dk>
2014-10-30 08:16:41 +0100

    removed "--" in front of Formula to improve future compatibility with UPPAAL

--- src/dk/aau/cs/verification/UPPAAL/VerifytaOutputParser.java
+++ src/dk/aau/cs/verification/UPPAAL/VerifytaOutputParser.java
@@ -8,8 +8,8 @@ import dk.aau.cs.verification.QueryType;
 public class VerifytaOutputParser {
 	private static final String PROPERTY_IS_NOT_SATISFIED_STRING = "Property is NOT satisfied";
 	private static final String PROPERTY_IS_SATISFIED_STRING = "Property is satisfied";
-	private static final String PROPERTY_IS_NOT_SATISFIED_STRING_NEW = "-- Formula is NOT satisfied";
-	private static final String PROPERTY_IS_SATISFIED_STRING_NEW = "-- Formula is satisfied";
+	private static final String PROPERTY_IS_NOT_SATISFIED_STRING_NEW = "Formula is NOT satisfied";
+	private static final String PROPERTY_IS_SATISFIED_STRING_NEW = "Formula is satisfied";
 	private static final String DISCRETE_INCLUSION = "discrete inclusion";
 	private boolean error = false;
 	private boolean discreteInclusion = false;
