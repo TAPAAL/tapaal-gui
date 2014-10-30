5ab0fabb140a15e7f0813e87a68463886d572aa7
Jiri Srba <srba@cs.aau.dk>
2014-10-30 08:21:05 +0100

    further improved compatibility with UPPAAL engine

--- src/dk/aau/cs/verification/UPPAAL/VerifytaOutputParser.java
+++ src/dk/aau/cs/verification/UPPAAL/VerifytaOutputParser.java
@@ -6,10 +6,8 @@ import dk.aau.cs.verification.QueryResult;
 import dk.aau.cs.verification.QueryType;
 
 public class VerifytaOutputParser {
-	private static final String PROPERTY_IS_NOT_SATISFIED_STRING = "Property is NOT satisfied";
-	private static final String PROPERTY_IS_SATISFIED_STRING = "Property is satisfied";
-	private static final String PROPERTY_IS_NOT_SATISFIED_STRING_NEW = "Formula is NOT satisfied";
-	private static final String PROPERTY_IS_SATISFIED_STRING_NEW = "Formula is satisfied";
+	private static final String PROPERTY_IS_NOT_SATISFIED_STRING = "is NOT satisfied";
+	private static final String PROPERTY_IS_SATISFIED_STRING = "is satisfied";
 	private static final String DISCRETE_INCLUSION = "discrete inclusion";
 	private boolean error = false;
 	private boolean discreteInclusion = false;
@@ -29,9 +27,9 @@ public class VerifytaOutputParser {
 			for (int i = 0; i < lines.length; i++) {
 				String line = lines[i];
 				if (line.contains(DISCRETE_INCLUSION)) { discreteInclusion = true;}
-				if (line.contains(PROPERTY_IS_SATISFIED_STRING) || line.contains(PROPERTY_IS_SATISFIED_STRING_NEW)) {
+				if (line.contains(PROPERTY_IS_SATISFIED_STRING)) {
 					return new QueryResult(true, new InconclusiveBoundednessAnalysisResult(), query, discreteInclusion);
-				} else if (line.contains(PROPERTY_IS_NOT_SATISFIED_STRING) || line.contains(PROPERTY_IS_NOT_SATISFIED_STRING_NEW)) {
+				} else if (line.contains(PROPERTY_IS_NOT_SATISFIED_STRING)) {
 					return new QueryResult(false, new InconclusiveBoundednessAnalysisResult(), query, discreteInclusion);
 				}
 			}
