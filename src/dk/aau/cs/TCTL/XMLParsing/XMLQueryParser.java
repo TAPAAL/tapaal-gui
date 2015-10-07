664e483c799c0e8b92357e146359c587eda57a14
Mads Johannsen <mjohan12@student.aau.dk>
2015-10-07 23:36:54 +0200

    Added more helper functions and parsing rules

--- src/dk/aau/cs/TCTL/XMLParsing/XMLQueryParser.java
+++ src/dk/aau/cs/TCTL/XMLParsing/XMLQueryParser.java
@@ -1,6 +1,7 @@
 package dk.aau.cs.TCTL.XMLParsing;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import dk.aau.cs.TCTL.AritmeticOperator;
 import dk.aau.cs.TCTL.TCTLPlusListNode;
@@ -22,6 +23,7 @@ import dk.aau.cs.TCTL.TCTLTrueNode;
 import dk.aau.cs.TCTL.TCTLDeadlockNode;
 
 import javax.xml.parsers.DocumentBuilder;
+
 import org.xml.sax.SAXException;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
@@ -44,31 +46,120 @@ public class XMLQueryParser {
 
     private TCTLAbstractProperty parseFormula(Node formula)
         throws XMLQueryParseException{
-        /* We only support EF and AG formulas. It is therefore
-         * unnecessary to check for operators F and G.
-         */
-        Node pathQuantifier = getFirstChildNode(formula);
+
         TCTLAbstractStateProperty booleanFormula = null;
+        Node operator = getFirstChildNode(formula);
+        Node child = getFirstChildNode(operator);
+        String pathOperator = null;
 
-        // Skip tags <finally> or <globally>
-        Node child = getFirstChildNode(getFirstChildNode(pathQuantifier));
-        
-
-        switch(pathQuantifier.getNodeName()){
-            case "exists-path":
+        switch(operator.getNodeName()){
+            /* TODO create new reachability operators for the MCC14 queries
+            // MCC14 Reachability operators
+            case "invariant":
                 booleanFormula = parseBooleanFormula(child);
-                return new TCTLEFNode(booleanFormula);
+                return new TCTLInvariantNode(booleanFormula);
+
+            case "impossibility":
+                booleanFormula = parseBooleanFormula(child);
+                return new TCTLImpossibilityNode(booleanFormula);
+
+            case "possibility":
+                booleanFormula = parseBooleanFormula(child);
+                return new TCTLPossibilityNode(booleanFormula);*/
+
+            // State operators
+            case "exists-path":
+                pathOperator = getFirstChildNode(child).getNodeName();
+                if (pathOperator.equals("finally")){
+                    booleanFormula = parseBooleanFormula(getFirstChildNode(child));
+                    return new TCTLEFNode(booleanFormula);
+                } else{
+                    throw new XMLQueryParseException("Invalid path operator: " + pathOperator);
+                }
 
             case "all-paths":
-                booleanFormula = parseBooleanFormula(child);
-                return new TCTLAGNode(booleanFormula);
+                pathOperator = getFirstChildNode(child).getNodeName();
+                if (pathOperator.equals("globally")){
+                    booleanFormula = parseBooleanFormula(getFirstChildNode(child));
+                    return new TCTLAGNode(booleanFormula);
+                } else{
+                    throw new XMLQueryParseException("Invalid path operator: " + pathOperator);
+                }
         }
-        throw new XMLQueryParseException("No path quantifier found");
+        throw new XMLQueryParseException("No state operator found");
     }
 
     private TCTLAbstractStateProperty parseBooleanFormula(Node booleanFormula)
         throws XMLQueryParseException{
-        //TODO continue from here
+
+        String nodeName = booleanFormula.getNodeName();
+
+        switch(nodeName){
+            case "deadlock":
+                return new TCTLDeadlockNode();
+
+            case "true":
+                return new TCTLTrueNode();
+
+            case "false":
+                return new TCTLFalseNode();
+
+            case "negation":
+                if(getChildCount(booleanFormula) == 1){
+                    TCTLAbstractStateProperty booleanFormulaChild =
+                        parseBooleanFormula(getFirstChildNode(booleanFormula));
+                    return new TCTLNotNode(booleanFormulaChild);
+                } else{
+                    throw new XMLQueryParseException("Invalid negation");
+                }
+
+            case "conjunction":
+            case "disjunction":
+                if(getChildCount(booleanFormula) < 2){
+                    throw new XMLQueryParseException("Invalid use of: " 
+                        + nodeName);
+                }
+
+                ArrayList<Node> children = getAllChildren(booleanFormula);
+                ArrayList<TCTLAbstractStateProperty> properties = 
+                    new ArrayList<TCTLAbstractStateProperty>();
+
+                for (Node n : children){
+                    properties.add(parseBooleanFormula(n));
+                }
+
+                if(nodeName == "conjunction"){
+                    return new TCTLAndListNode(properties);
+                } else{
+                    return new TCTLOrListNode(properties);
+                }
+
+            case "exclusive-disjunction":
+            case "implication":
+            case "equivalence":
+
+            case "integer-eq":
+            case "integer-ne":
+            case "integer-lt":
+            case "integer-le":
+            case "integer-gt":
+            case "integer-ge":
+
+            case "is-fireable":
+
+        }
+    }
+
+    private TCTLConstNode parseIntegerExpression(Node integerExpression){
+        switch (integerExpression.getNodeName()){
+            case "integer-constant":
+            case "tokens-count":
+
+            case "integer-sum":
+            case "integer-product":
+
+            case "integer-difference":
+        }
     }
 
     public static Node findSubNode(String name, Node node){
@@ -93,6 +184,20 @@ public class XMLQueryParser {
         return null;
     }
 
+    private ArrayList<Node> getAllChildren(Node parentNode) {
+        NodeList children = parentNode.getChildNodes();
+        ArrayList<Node> elementNodes = new ArrayList<Node>();
+
+        for (int i = 0; i < children.getLength(); i++) {
+            Node node = children.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                elementNodes.add(node);
+            }
+        }
+
+        return elementNodes;
+    }
+
     public String getText(Node node){
         String result = null;
 
@@ -106,6 +211,25 @@ public class XMLQueryParser {
             Node subNode = children.item(i);
             if (subNode.getNodeType() == Node.TEXT_NODE) {
                 result = subNode.getNodeValue();
+            }
+        }
+
+       return result;
+    }
+
+    public int getChildCount(Node node){
+        int result = 0;
+
+        if (!node.hasChildNodes()){
+            return result;
+        }
+
+        NodeList children = node.getChildNodes();
+        
+        for (int i = 0; i < children.getLength(); i++){
+            Node subNode = children.item(i);
+            if (subNode.getNodeType() == Node.ELEMENT_NODE) {
+                result++;
             }
         }
 
