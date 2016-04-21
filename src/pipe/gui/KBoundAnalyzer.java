33c88426c2398cde3b2b6060e85c610e9a3a3ac9
Mads Johannsen <mjohan12@student.aau.dk>
2016-04-21 21:11:59 +0200

    Added XML output methods

--- src/pipe/gui/KBoundAnalyzer.java
+++ src/pipe/gui/KBoundAnalyzer.java
@@ -5,6 +5,7 @@ import javax.swing.JSpinner;
 import pipe.dataLayer.TAPNQuery.SearchOption;
 import pipe.dataLayer.TAPNQuery.TraceOption;
 import pipe.dataLayer.TAPNQuery.AlgorithmOption;
+import pipe.dataLayer.TAPNQuery.QueryCategory;
 import pipe.gui.widgets.RunningVerificationDialog;
 import dk.aau.cs.Messenger;
 import dk.aau.cs.TCTL.TCTLAGNode;
@@ -52,7 +53,7 @@ public class KBoundAnalyzer {
 
 	protected VerifyTAPNOptions verificationOptions() {
 		if(modelChecker instanceof VerifyPN){
-			return new VerifyPNOptions(k, TraceOption.NONE, SearchOption.BFS, false, ModelReduction.BOUNDPRESERVING, false, false, 1, AlgorithmOption.CERTAIN_ZERO);
+			return new VerifyPNOptions(k, TraceOption.NONE, SearchOption.BFS, false, ModelReduction.BOUNDPRESERVING, false, false, 1, QueryCategory.Default, AlgorithmOption.CERTAIN_ZERO);
 		} else if(modelChecker instanceof VerifyTAPN){
 			return new VerifyTAPNOptions(k, TraceOption.NONE, SearchOption.BFS, true, false, true, false, false, 1);
 		} else if(modelChecker instanceof VerifyTAPNDiscreteVerification){
