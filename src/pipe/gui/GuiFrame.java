c5310a7bc88ba5f3eef8c54aa0c2002c8c17c054
Jakob Dyhr <jdyhr12@student.aau.dk>
2016-11-11 20:00:01 +0100

    Moved clutter from guiframe to new class

--- src/pipe/gui/GuiFrame.java
+++ src/pipe/gui/GuiFrame.java
@@ -51,6 +51,7 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 import com.sun.jdi.connect.Transport;
+import dk.aau.cs.gui.TabTransformer;
 import dk.aau.cs.model.tapn.*;
 import net.tapaal.Preferences;
 import com.sun.jna.Platform;
@@ -1551,11 +1552,9 @@ public class GuiFrame extends JFrame implements Observer {
 		setTitle(name);// Change the program caption
 		appTab.setTitleAt(freeSpace, name);
 		selectAction.actionPerformed(null);
-
-
 	}
 
-	public void duplicateTab(TabContent tabToDuplicate) {
+	private void duplicateTab(TabContent tabToDuplicate) {
 		int index = appTab.indexOfComponent(tabToDuplicate);
 
 		NetWriter tapnWriter = new TimedArcPetriNetNetworkWriter(
@@ -1576,137 +1575,9 @@ public class GuiFrame extends JFrame implements Observer {
 		}
 	}
 
-	public void convertToUntimedTab(TabContent tab){
-		for(Template template : tab.allTemplates()){
-			ArrayList<TimedTransportArcComponent> transportArcComponents = new ArrayList<>();
-			// Make place token age invariant infinite
-			for(TimedPlace place : template.model().places()){
-				place.setInvariant(TimeInvariant.LESS_THAN_INFINITY);
-			}
-			// Make transitions non-urgent
-			for(TimedTransition transition : template.model().transitions()){
-				transition.setUrgent(false);
-			}
-
-			for(Arc arc : template.guiModel().getArcs()){
-				// Make output arc guards infinite
-				if(arc instanceof TimedOutputArcComponent) {
-					TimedOutputArcComponent arcComp = (TimedOutputArcComponent) arc;
-					arcComp.setGuardAndWeight(TimeInterval.ZERO_INF, arcComp.getWeight());
-				}
-
-				// Add and process transport arcs in separate list to avoid delete errors
-				if(arc instanceof TimedTransportArcComponent){
-					TimedTransportArcComponent arcComp = (TimedTransportArcComponent) arc;
-					transportArcComponents.add(arcComp);
-				}
-			}
-
-			// Replace transport arcs with regular arcs
-			for(TimedTransportArcComponent arc : transportArcComponents){
-				// Input arc
-				if(arc.getSource() instanceof Place) {
-					TimedPlace source = template.model().getPlaceByName(arc.getSource().getName());
-					TimedTransition destination = template.model().getTransitionByName(arc.getTarget().getName());
-
-					TimedInputArc addedArc = new TimedInputArc(source, destination, TimeInterval.ZERO_INF, arc.getWeight());
-					template.model().add(addedArc);
-
-					// GUI
-					DataLayer guiModel = template.guiModel();
-					Place guiSource = guiModel.getPlaceByName(arc.getSource().getName());
-					Transition guiTarget = guiModel.getTransitionByName(arc.getTarget().getName());
-					Arc newArc = new TimedInputArcComponent(new TimedOutputArcComponent(
-							0d,
-							0d,
-							0d,
-							0d,
-							guiSource,
-							guiTarget,
-							arc.getWeight().value(),
-							arc.getSource().getName() + "_to_" + arc.getTarget().getName(),
-							false
-					));
-
-					// Build ArcPath
-					Place oldGuiSource = guiModel.getPlaceByName(arc.getSource().getName());
-					Transition oldGuiTarget = guiModel.getTransitionByName(arc.getTarget().getName());
-					ArcPath newArcPath = createArcPath(guiModel, oldGuiSource, oldGuiTarget, newArc);
-
-					// Set arcPath, guiModel and connectors
-					((TimedInputArcComponent) newArc).setUnderlyingArc(addedArc);
-					newArc.setArcPath(newArcPath);
-					newArc.setGuiModel(guiModel);
-					newArc.updateArcPosition();
-					guiModel.addPetriNetObject(newArc);
-					guiSource.addConnectFrom(newArc);
-					guiTarget.addConnectTo(newArc);
-
-				}
-				// Output arc
-				else if(arc.getSource() instanceof Transition){
-					TimedPlace destination = template.model().getPlaceByName(arc.getTarget().getName());
-					TimedTransition source = template.model().getTransitionByName(arc.getSource().getName());
-
-					TimedOutputArc addedArc = new TimedOutputArc(source, destination, arc.getWeight());
-					template.model().add(addedArc);
-
-					// GUI
-					DataLayer guiModel = template.guiModel();
-					Place guiTarget = guiModel.getPlaceByName(arc.getTarget().getName());
-					Transition guiSource = guiModel.getTransitionByName(arc.getSource().getName());
-					Arc newArc = new TimedOutputArcComponent(
-							0d,
-							0d,
-							0d,
-							0d,
-							guiSource,
-							guiTarget,
-							arc.getWeight().value(),
-							arc.getSource().getName() + "_to_" + arc.getTarget().getName(),
-							false
-					);
-
-					// Build ArcPath
-					Place oldGuiTarget = guiModel.getPlaceByName(arc.getTarget().getName());
-					Transition oldGuiSource = guiModel.getTransitionByName(arc.getSource().getName());
-					ArcPath newArcPath = createArcPath(guiModel, oldGuiSource, oldGuiTarget, newArc);
-
-					// Set arcPath, guiModel and connectors
-					((TimedOutputArcComponent) newArc).setUnderlyingArc(addedArc);
-					newArc.setArcPath(newArcPath);
-					newArc.setGuiModel(guiModel);
-					newArc.updateArcPosition();
-					guiModel.addPetriNetObject(newArc);
-					guiSource.addConnectFrom(newArc);
-					guiTarget.addConnectTo(newArc);
-
-					// Delete the transport arc
-					arc.delete();
-				}
-			}
-
-			setGUIMode(GUIMode.draw);
-		}
-	}
-
-	private ArcPath createArcPath(DataLayer currentGuiModel, PlaceTransitionObject source, PlaceTransitionObject target, Arc arc) {
-		Arc guiArc = currentGuiModel.getArcByEndpoints(source, target);
-		ArcPath arcPath = guiArc.getArcPath();
-		int arcPathPointsNum = arcPath.getNumPoints();
-
-		// Build ArcPath
-		ArcPath newArcPath = new ArcPath(arc);
-		for(int k = 0; k < arcPathPointsNum; k++) {
-			ArcPathPoint point = arcPath.getArcPathPoint(k);
-			newArcPath.addPoint(
-					point.getPoint().x,
-					point.getPoint().y,
-					point.getPointType()
-			);
-		}
-
-		return newArcPath;
+	private void convertToUntimedTab(TabContent tab){
+		TabTransformer.removeTimingInformation(tab);
+		setGUIMode(GuiFrame.GUIMode.draw);
 	}
 
 	private void undoAddTab(int currentlySelected) {
