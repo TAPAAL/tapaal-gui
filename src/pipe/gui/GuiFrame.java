d9a7cac07c148b0be74cc0c1deea91737015b029
Kenneth Yrke Jørgensen <kenneth@yrke.dk>
2018-05-01 17:07:35 +0200

    Fixed getJRE version for java 9+

--- src/pipe/gui/GuiFrame.java
+++ src/pipe/gui/GuiFrame.java
@@ -177,7 +177,21 @@ public class GuiFrame extends JFrame implements Observer {
 	}
 
 	public int getJRE(){
-		return Character.getNumericValue(System.getProperty("java.version").charAt(2));
+		String version = System.getProperty("java.version");
+		String[] versionSplit = version.split("\\.");
+
+		try {
+			if (Integer.parseInt(versionSplit[0]) >= 9) {
+				//Version format (9.X.Y)
+				return Integer.parseInt(versionSplit[0]);
+			} else {
+				//Before java 9 version in format (1.X.Y)
+				return Integer.parseInt(versionSplit[1]);
+			}
+		} catch (NumberFormatException e) {
+			Logger.log("Error parsing java version, failing silent (0): " + e.getMessage());
+			return 0; // Unknown version
+		}
 	}	
 
 	public GuiFrame(String title) {
