9dcdb501771801a9de72df7350f84842035214a9
Peter Taankvist ptaankvist@gmail.com <Peter Taankvist ptaankvist@gmail.com>
2019-03-02 12:47:27 +0100

    return false on no option, so nothing is done

--- src/pipe/gui/widgets/PlaceEditorPanel.java
+++ src/pipe/gui/widgets/PlaceEditorPanel.java
@@ -720,6 +720,8 @@ public class PlaceEditorPanel extends javax.swing.JPanel {
 							Command cmd = new MakePlaceNewSharedMultiCommand(context, newName, place);	
 							cmd.redo();
 							context.undoManager().addNewEdit(cmd);
+						} else {
+							return false;
 						}
 					} else {
 						JOptionPane.showMessageDialog(this, "A transition with the specified name already exists in one or more components, or the specified name is invalid.\n\nAcceptable names for places are defined by the regular expression:\n[a-zA-Z][_a-zA-Z0-9]*\n\nNote that \"true\" and \"false\" are reserved keywords.", "Error", JOptionPane.OK_OPTION);
