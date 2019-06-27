0965fe839c5f9a02882179c9c460ffff6babec38
Kenneth Yrke Joergensen <kenneth@yrke.dk>
2019-06-27 10:35:41 +0200

    AddTimedInputArcCommand no longer uses delete/undelete

--- src/pipe/gui/undo/AddTimedInputArcCommand.java
+++ src/pipe/gui/undo/AddTimedInputArcCommand.java
@@ -8,20 +8,31 @@ import dk.aau.cs.model.tapn.TimedArcPetriNet;
 public class AddTimedInputArcCommand extends TAPNElementCommand {
 	private final TimedInputArcComponent timedArc;
 
-	public AddTimedInputArcCommand(TimedInputArcComponent timedArc,
-			TimedArcPetriNet tapn, DataLayer guiModel) {
+	public AddTimedInputArcCommand(TimedInputArcComponent timedArc, TimedArcPetriNet tapn, DataLayer guiModel) {
 		super(tapn, guiModel);
 		this.timedArc = timedArc;
 	}
 
 	@Override
 	public void undo() {
-		timedArc.delete();
+		timedArc.underlyingTimedInputArc().delete();
+
+		//XXX: Should properly be part of the guiModel
+		if (timedArc.getSource() != null) timedArc.getSource().removeFromArc(timedArc);
+		if (timedArc.getTarget() != null) timedArc.getTarget().removeToArc(timedArc);
+
+		guiModel.removePetriNetObject(timedArc);
 	}
 
 	@Override
 	public void redo() {
 		timedArc.undelete();
+		guiModel.addPetriNetObject(timedArc);
+
+		//XXX: Should properly be part of the guiModel
+		if (timedArc.getSource() != null) timedArc.getSource().addConnectFrom(timedArc);
+		if (timedArc.getTarget() != null) timedArc.getTarget().addConnectTo(timedArc);
+
 		tapn.add(timedArc.underlyingTimedInputArc());
 	}
 }
