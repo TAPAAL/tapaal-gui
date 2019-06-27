153b8ce7ab0ac5223e91b23f3bf341f5789d3053
Kenneth Yrke Joergensen <kenneth@yrke.dk>
2019-06-27 10:14:20 +0200

    AddTimedOutputArcCommand no longer uses delete/undelete

--- src/pipe/gui/undo/AddTimedOutputArcCommand.java
+++ src/pipe/gui/undo/AddTimedOutputArcCommand.java
@@ -8,20 +8,30 @@ import dk.aau.cs.model.tapn.TimedArcPetriNet;
 public class AddTimedOutputArcCommand extends TAPNElementCommand {
 	private final TimedOutputArcComponent outputArc;
 
-	public AddTimedOutputArcCommand(TimedOutputArcComponent outputArc,
-			TimedArcPetriNet tapn, DataLayer guiModel) {
+	public AddTimedOutputArcCommand(TimedOutputArcComponent outputArc, TimedArcPetriNet tapn, DataLayer guiModel) {
 		super(tapn, guiModel);
 		this.outputArc = outputArc;
 	}
 
 	@Override
 	public void undo() {
-		outputArc.delete();
+		outputArc.underlyingArc().delete();
+
+		//XXX: Should properly be part of the guiModel
+		if (outputArc.getSource() != null) outputArc.getSource().removeFromArc(outputArc);
+		if (outputArc.getTarget() != null) outputArc.getTarget().removeToArc(outputArc);
+
+		guiModel.removePetriNetObject(outputArc);
 	}
 
 	@Override
 	public void redo() {
-		outputArc.undelete();
+		guiModel.addPetriNetObject(outputArc);
+
+		//XXX: Should properly be part of the guiModel
+		if (outputArc.getSource() != null) outputArc.getSource().addConnectFrom(outputArc);
+		if (outputArc.getTarget() != null) outputArc.getTarget().addConnectTo(outputArc);
+
 		tapn.add(outputArc.underlyingArc());
 	}
 
