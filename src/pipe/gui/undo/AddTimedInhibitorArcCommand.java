e2c51e2b605d179580d9050c8dfc01e05908588d
Kenneth Yrke Joergensen <kenneth@yrke.dk>
2019-06-27 10:49:04 +0200

    AddInhibitorArc no longer uses delete/undelete

--- src/pipe/gui/undo/AddTimedInhibitorArcCommand.java
+++ src/pipe/gui/undo/AddTimedInhibitorArcCommand.java
@@ -8,20 +8,31 @@ import dk.aau.cs.model.tapn.TimedArcPetriNet;
 public class AddTimedInhibitorArcCommand extends TAPNElementCommand {
 	private final TimedInhibitorArcComponent inhibitorArc;
 
-	public AddTimedInhibitorArcCommand(TimedInhibitorArcComponent inhibitorArc,
-			TimedArcPetriNet tapn, DataLayer guiModel) {
+	public AddTimedInhibitorArcCommand(TimedInhibitorArcComponent inhibitorArc, TimedArcPetriNet tapn, DataLayer guiModel) {
 		super(tapn, guiModel);
 		this.inhibitorArc = inhibitorArc;
 	}
 
 	@Override
 	public void undo() {
-		inhibitorArc.delete();
+		inhibitorArc.underlyingTimedInhibitorArc().delete();
+
+		//XXX: Should properly be part of the guiModel
+		if (inhibitorArc.getSource() != null) inhibitorArc.getSource().removeFromArc(inhibitorArc);
+		if (inhibitorArc.getTarget() != null) inhibitorArc.getTarget().removeToArc(inhibitorArc);
+
+		guiModel.removePetriNetObject(inhibitorArc);
 	}
 
 	@Override
 	public void redo() {
-		inhibitorArc.undelete();
+		guiModel.addPetriNetObject(inhibitorArc);
+
+		//XXX: Should properly be part of the guiModel
+		if (inhibitorArc.getSource() != null) inhibitorArc.getSource().addConnectFrom(inhibitorArc);
+		if (inhibitorArc.getTarget() != null) inhibitorArc.getTarget().addConnectTo(inhibitorArc);
+
+
 		tapn.add(inhibitorArc.underlyingTimedInhibitorArc());
 	}
 
