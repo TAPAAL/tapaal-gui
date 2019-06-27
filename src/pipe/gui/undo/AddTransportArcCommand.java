a76f6027a89c3f5eed87c0d14bfb38e262ae1592
Kenneth Yrke Joergensen <kenneth@yrke.dk>
2019-06-27 11:50:44 +0200

    AddTransportArcCommand no longer uses delete/undelete

--- src/pipe/gui/undo/AddTransportArcCommand.java
+++ src/pipe/gui/undo/AddTransportArcCommand.java
@@ -10,9 +10,7 @@ public class AddTransportArcCommand extends TAPNElementCommand {
 	private final TimedTransportArcComponent transportArcComponent;
 	private final TransportArc transportArc;
 
-	public AddTransportArcCommand(TimedTransportArcComponent transportArcComponent,
-			TransportArc transportArc, TimedArcPetriNet tapn,
-			DataLayer guiModel) {
+	public AddTransportArcCommand(TimedTransportArcComponent transportArcComponent, TransportArc transportArc, TimedArcPetriNet tapn, DataLayer guiModel) {
 		super(tapn, guiModel);
 		this.transportArcComponent = transportArcComponent;
 		this.transportArc = transportArc;
@@ -20,14 +18,38 @@ public class AddTransportArcCommand extends TAPNElementCommand {
 
 	@Override
 	public void undo() {
-		transportArcComponent.delete();
+		transportArc.delete();
+
+		TimedTransportArcComponent partner = transportArcComponent.getConnectedTo();
+
+		//XXX: Should properly be part of the guiModel
+		if (transportArcComponent.getSource() != null) transportArcComponent.getSource().removeFromArc(transportArcComponent);
+		if (transportArcComponent.getTarget() != null) transportArcComponent.getTarget().removeToArc(transportArcComponent);
+
+		if (partner.getSource() != null) partner.getSource().removeFromArc(partner);
+		if (partner.getTarget() != null) partner.getTarget().removeToArc(partner);
+
+
+		guiModel.removePetriNetObject(transportArcComponent);
+		guiModel.removePetriNetObject(partner);
+
+
 	}
 
 	@Override
 	public void redo() {
-		transportArcComponent.setUnderlyingArc(transportArc);
-		transportArcComponent.getConnectedTo().setUnderlyingArc(transportArc);
-		transportArcComponent.undelete();
+		TimedTransportArcComponent partner = transportArcComponent.getConnectedTo();
+
+		guiModel.addPetriNetObject(transportArcComponent);
+		guiModel.addPetriNetObject(partner);
+
+		//XXX: Should properly be part of the guiModel
+		if (transportArcComponent.getSource() != null) transportArcComponent.getSource().addConnectFrom(transportArcComponent);
+		if (transportArcComponent.getTarget() != null) transportArcComponent.getTarget().addConnectTo(transportArcComponent);
+
+		if (partner.getSource() != null) partner.getSource().addConnectFrom(partner);
+		if (partner.getTarget() != null) partner.getTarget().addConnectTo(partner);
+
 		tapn.add(transportArc);
 	}
 }
