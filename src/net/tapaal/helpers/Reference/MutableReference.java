672b2939a72a07258987712736a8a09184157b3d
Kenneth Yrke Joergensen <kenneth@yrke.dk>
2019-07-21 11:05:35 +0200

    Reference now is class not interface and accpets map and isPresent

--- src/net/tapaal/helpers/Reference/MutableReference.java
+++ src/net/tapaal/helpers/Reference/MutableReference.java
@@ -6,21 +6,14 @@ import dk.aau.cs.util.Require;
  * Represents an reference to an object, what can be updated
  * Never pass the mutablereference to an other object, pass read only Reference
  */
-public class MutableReference<T> implements Reference<T> {
-
-    T ref;
+public class MutableReference<T> extends Reference<T> {
 
     public MutableReference(T ref) {
-        setReference(ref);
+        super(ref);
     }
 
     public void setReference(T ref) {
         Require.notNull(ref, "Can't make a reference to null");
-        this.ref = ref;
-    }
-
-    @Override
-    public T get() {
-        return ref;
+        super.setReference(ref);
     }
 }
--- src/net/tapaal/helpers/Reference/Reference.java
+++ src/net/tapaal/helpers/Reference/Reference.java
@@ -1,7 +1,35 @@
 package net.tapaal.helpers.Reference;
 
-public interface Reference<T> {
+import dk.aau.cs.util.Require;
 
-    T get();
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Reference<T> {
+
+    private T ref;
+
+    public Reference(T ref) {
+        setReference(ref);
+    }
+    public final T get() {
+        return ref;
+    }
+
+    void setReference(T ref) {
+        Require.notNull(ref, "Can't make a reference to null");
+        this.ref = ref;
+    }
+
+    public void ifPresent(Consumer<? super T> consumer) {
+        if (ref != null)
+            consumer.accept(ref);
+    }
+
+    public<U> U map(Function<? super T, ? extends U> mapper) {
+        Objects.requireNonNull(mapper);
+        return mapper.apply(ref);
+    }
 
 }
