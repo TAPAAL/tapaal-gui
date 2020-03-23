d313244556b37a2a0bca27ede96bfa03b10489ed
ptaankvist@gmail.com <ptaankvist@gmail.com>
2020-03-23 20:48:04 +0100

    Buttons correctly disabled if PNML import fails and there are no other nets open. Also do not show SmartDrawDialog if PNML import fails

--- src/pipe/gui/CreateGui.java
+++ src/pipe/gui/CreateGui.java
@@ -97,4 +97,8 @@ public class CreateGui {
 	public static GuiFrame getAppGui() {
 		return appGui;
 	}
+        
+        public static int getTabsSize(){
+            return tabs.size();
+        }
 }
