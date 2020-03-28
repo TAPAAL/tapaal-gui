3d94ccb9077b4765ca5b649d3ba311181f0b973a
Kenneth Yrke Joergensen <kenneth@yrke.dk>
2020-03-28 12:32:40 +0100

    Enabled Kotlin and upgraded tests to Junit5

--- tests/TimeIntervalTest.java
+++ tests/TimeIntervalTest.java
@@ -1,35 +1,37 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import dk.aau.cs.model.tapn.Bound;
 import dk.aau.cs.model.tapn.IntBound;
 import dk.aau.cs.model.tapn.TimeInterval;
 import dk.aau.cs.util.RequireException;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TimeIntervalTest  {
 	
-	@Test(expected=RequireException.class)
+	@Test
 	public void TimeIntervalConstructor(){
-		new TimeInterval(false, new IntBound(0), new IntBound(0), true);
+		assertThrows(RequireException.class, ()-> new TimeInterval(false, new IntBound(0), new IntBound(0), true));
 	}
 	
-	@Test(expected=RequireException.class)
+	@Test
 	public void TimeIntervalConstructor_intervaltype2(){
-		new TimeInterval(true, new IntBound(5), new IntBound(5), false);
+		assertThrows(RequireException.class, ()-> new TimeInterval(true, new IntBound(5), new IntBound(5), false));
 	}
 	
-	@Test(expected=RequireException.class)
+	@Test
 	public void TimeIntervalConstructor_intervaltype3(){
-		new TimeInterval(false, new IntBound(5), new IntBound(5), false);
+		assertThrows(RequireException.class, ()-> new TimeInterval(false, new IntBound(5), new IntBound(5), false));
 	}
 	
-	@Test(expected=RequireException.class)
+	@Test
 	public void TimeIntervalConstructor_intervaltype4(){
-		new TimeInterval(true, new IntBound(6), new IntBound(5), false);
+		assertThrows(RequireException.class, ()-> new TimeInterval(true, new IntBound(6), new IntBound(5), false));
 	}
 	
-	@Test(expected=RequireException.class)
+	@Test
 	public void TimeIntervalConstructor_intervaltype5(){
-		new TimeInterval(false, new IntBound(6), new IntBound(5), false);
+		assertThrows(RequireException.class, ()-> new TimeInterval(false, new IntBound(6), new IntBound(5), false));
 	}
 	
 	@Test
@@ -37,14 +39,14 @@ public class TimeIntervalTest  {
 		new TimeInterval(true, new IntBound(6), Bound.Infinity, false);
 	}
 	
-	@Test(expected=RequireException.class)
+	@Test
 	public void TimeInterval_InfinityUpper_included(){
-		new TimeInterval(true, new IntBound(6), Bound.Infinity, true);
+		assertThrows(RequireException.class, ()-> new TimeInterval(true, new IntBound(6), Bound.Infinity, true));
 	}
 	
-	@Test(expected=RequireException.class)
+	@Test
 	public void TimeInterval_Infinity_lower(){
-		new TimeInterval(true, Bound.Infinity, Bound.Infinity, false);
+		assertThrows(RequireException.class, ()-> new TimeInterval(true, Bound.Infinity, Bound.Infinity, false));
 	}
 	
 	
