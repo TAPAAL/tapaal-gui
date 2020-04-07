0060fa968671400057b985cb6e47746e4e29d772
Kenneth Yrke Joergensen <kenneth@yrke.dk>
2020-04-07 12:00:11 +0200

    Added transition test

--- tests/dk/aau/cs/io/TapnXmlLoaderTest.kt
+++ tests/dk/aau/cs/io/TapnXmlLoaderTest.kt
@@ -5,9 +5,7 @@ import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.function.ThrowingSupplier
 
-fun  String.asInpurtStream() : java.io.InputStream {
-    return java.io.StringBufferInputStream(this)
-}
+
 
 internal class TapnXmlLoaderTest {
 
@@ -21,20 +19,10 @@ internal class TapnXmlLoaderTest {
         }
     }
 
-    class PlaceParse {
-        fun xmlNet(s:String) : String {
-            return """
-                <?xml version="1.0" encoding="UTF-8" standalone="no"?>
-                <pnml xmlns="http://www.informatik.hu-berlin.de/top/pnml/ptNetb">
-                  <net active="true" id="IntroExample" type="P/T net">
-                    $s
-                  </net>
-                 </pnml>
-            """.trimIndent()
-        }
+    class Place {
 
         @Test
-        fun `Parse valid place`() {
+        fun `Parse place`() {
             val net = xmlNet(
                 """
                     <place displayName="true" id="Start" initialMarking="1" invariant="&lt; inf" name="Start" nameOffsetX="-5" nameOffsetY="35" positionX="135" positionY="30"/>
@@ -60,7 +48,7 @@ internal class TapnXmlLoaderTest {
 
         @Test
         //Older version of TAPAAL saved the positionX/Y and nameOffsetX/Y in double format eg. 35.0
-        fun `Place positions can be doubles`(){
+        fun `Place positions can be double formatted`(){
 
             val net = xmlNet(
                 """
@@ -73,10 +61,8 @@ internal class TapnXmlLoaderTest {
                 tapnXmlLoader.load(net)
             })
 
-            Assertions.assertEquals(1, r.templates().first().guiModel().places.size)
-
             val place = r.templates().first().guiModel().places[0]
-            Assertions.assertEquals("Start", place.name)
+
             Assertions.assertEquals(135, place.positionX)
             Assertions.assertEquals(30, place.positionY)
 
@@ -88,4 +74,68 @@ internal class TapnXmlLoaderTest {
 
     }
 
+    class Transition {
+        @Test
+        fun `Parse Transition`() {
+            val net = xmlNet(
+                """
+                     <transition angle="90" displayName="true" id="T1" infiniteServer="false" name="T1" nameOffsetX="-5" nameOffsetY="35" positionX="360" positionY="300" priority="0" urgent="false"/>
+                """
+            ).asInpurtStream()
+            val tapnXmlLoader = TapnXmlLoader();
+
+            val r = Assertions.assertDoesNotThrow(ThrowingSupplier {
+                tapnXmlLoader.load(net)
+            })
+
+            Assertions.assertEquals(1, r.templates().first().guiModel().transitions.size)
+
+            val transition = r.templates().first().guiModel().transitions[0]
+            Assertions.assertEquals("T1", transition.name)
+            Assertions.assertEquals(360, transition.positionX)
+            Assertions.assertEquals(300, transition.positionY)
+
+            Assertions.assertEquals(-5, transition.nameOffsetX)
+            Assertions.assertEquals(35, transition.nameOffsetY)
+
+        }
+
+        @Test
+        //Older version of TAPAAL saved the positionX/Y and nameOffsetX/Y in double format eg. 35.0
+        fun `Transiton positions can be double formatted`(){
+            val net = xmlNet(
+                """
+                     <transition angle="90" displayName="true" id="T1" infiniteServer="false" name="T1" nameOffsetX="-5" nameOffsetY="35" positionX="360" positionY="300" priority="0" urgent="false"/>
+                """
+            ).asInpurtStream()
+            val tapnXmlLoader = TapnXmlLoader();
+
+            val r = Assertions.assertDoesNotThrow(ThrowingSupplier {
+                tapnXmlLoader.load(net)
+            })
+
+            val transition = r.templates().first().guiModel().transitions[0]
+            Assertions.assertEquals(360, transition.positionX)
+            Assertions.assertEquals(300, transition.positionY)
+
+            Assertions.assertEquals(-5, transition.nameOffsetX)
+            Assertions.assertEquals(35, transition.nameOffsetY)
+
+        }
+    }
+}
+
+fun  String.asInpurtStream() : java.io.InputStream {
+    return java.io.StringBufferInputStream(this)
+}
+
+fun xmlNet(s:String) : String {
+    return """
+                <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+                <pnml xmlns="http://www.informatik.hu-berlin.de/top/pnml/ptNetb">
+                  <net active="true" id="IntroExample" type="P/T net">
+                    $s
+                  </net>
+                 </pnml>
+            """.trimIndent()
 }
