0ebcfbda1c44219dc4ae6a43c22ba90419cb584a
Kenneth Yrke Joergensen <kenneth@yrke.dk>
2020-04-18 16:45:53 +0200

    Made members final

--- src/dk/aau/cs/util/Tuple.java
+++ src/dk/aau/cs/util/Tuple.java
@@ -1,8 +1,8 @@
 package dk.aau.cs.util;
 
 public class Tuple<T1, T2> {
-	private T1 value1;
-	private T2 value2;
+	private final T1 value1;
+	private final T2 value2;
 
 	public Tuple(T1 value1, T2 value2) {
 		this.value1 = value1;
