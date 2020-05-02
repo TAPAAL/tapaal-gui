686ead2785c03411f1bb9b0043233e0e95aea030
ptaankvist@gmail.com <ptaankvist@gmail.com>
2020-05-02 17:30:09 +0200

    Update drawingsurface when undoing/redoing movement of pnobjects

--- src/pipe/gui/undo/TranslatePetriNetObjectEdit.java
+++ src/pipe/gui/undo/TranslatePetriNetObjectEdit.java
@@ -4,6 +4,7 @@
 
 package pipe.gui.undo;
 
+import pipe.gui.canvas.DrawingSurfaceImpl;
 import pipe.gui.graphicElements.PetriNetObject;
 import dk.aau.cs.gui.undo.Command;
 
@@ -16,25 +17,29 @@ public class TranslatePetriNetObjectEdit extends Command {
 	PetriNetObject pnObject;
 	Integer transX;
 	Integer transY;
+	DrawingSurfaceImpl ds;
 
 	/** Creates a new instance of */
 	public TranslatePetriNetObjectEdit(PetriNetObject _pnObject,
-			Integer _transX, Integer _transY) {
+                                       Integer _transX, Integer _transY, DrawingSurfaceImpl drawingSurface) {
 		pnObject = _pnObject;
 		transX = _transX;
 		transY = _transY;
+		ds = drawingSurface;
 	}
 
 	/** */
 	@Override
 	public void undo() {
 		pnObject.translate(-transX, -transY);
+        ds.updatePreferredSize();
 	}
 
 	/** */
 	@Override
 	public void redo() {
 		pnObject.translate(transX, transY);
+        ds.updatePreferredSize();
 	}
 
 	@Override
