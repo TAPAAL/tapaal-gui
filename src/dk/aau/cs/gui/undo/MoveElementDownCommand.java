4a1fb701c1dc8dfa79f6adefb2cab867a7aac9ab
ptaankvist@gmail.com <ptaankvist@gmail.com>
2020-05-05 20:44:30 +0200

    Fix undo for arrows in sidepane

--- src/dk/aau/cs/gui/undo/MoveElementDownCommand.java
+++ src/dk/aau/cs/gui/undo/MoveElementDownCommand.java
@@ -10,37 +10,37 @@ public class MoveElementDownCommand extends Command{
     int newIndex;
     SidePane sidePane;
     JList<Template> templateList;
-    int currentIndex;
     public MoveElementDownCommand(SidePane sidePane, int oldIndex, int newIndex){
         this.oldIndex = oldIndex;
         this.newIndex = newIndex;
         this.sidePane = sidePane;
-        this.currentIndex = sidePane.getJList().getSelectedIndex();
     }
 
     @Override
     public void redo() {
-        if(currentIndex == newIndex){
-            sidePane.moveDown(newIndex);
-            sidePane.getJList().setSelectedIndex(newIndex+1);
-        }else if(currentIndex == newIndex+1){
-            sidePane.moveDown(newIndex);
-            sidePane.getJList().setSelectedIndex(oldIndex-1);
+        if(sidePane.getJList().getSelectedIndex() == oldIndex){
+            sidePane.moveDown(oldIndex);
+            sidePane.getJList().setSelectedIndex(oldIndex+1);
+        }else if(sidePane.getJList().getSelectedIndex() == oldIndex+1){
+            sidePane.moveDown(oldIndex);
+            sidePane.getJList().setSelectedIndex(newIndex-1);
         } else{
-            sidePane.moveDown(newIndex);
+            sidePane.moveDown(oldIndex);
         }
+        sidePane.getJList().updateUI();
     }
 
     @Override
     public void undo() {
-        if(currentIndex == oldIndex){
-            sidePane.moveUp(oldIndex);
-            sidePane.getJList().setSelectedIndex(oldIndex-1);
-        } else if(currentIndex == oldIndex-1){
-            sidePane.moveUp(oldIndex);
-            sidePane.getJList().setSelectedIndex(newIndex+1);
+        if(sidePane.getJList().getSelectedIndex() == newIndex){
+            sidePane.moveUp(newIndex);
+            sidePane.getJList().setSelectedIndex(newIndex-1);
+        } else if(sidePane.getJList().getSelectedIndex() == newIndex-1){
+            sidePane.moveUp(newIndex);
+            sidePane.getJList().setSelectedIndex(oldIndex+1);
         }else{
-            sidePane.moveUp(oldIndex);
+            sidePane.moveUp(newIndex);
         }
+        sidePane.getJList().updateUI();
     }
 }
