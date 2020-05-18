ab83731f81054d7dfdaf0af5932785d0f755d163
Kenneth Yrke Joergensen <kenneth@yrke.dk>
2020-05-18 16:15:24 +0200

    Converted interal function to lambda

--- src/pipe/gui/AnimationSettingsDialog.java
+++ src/pipe/gui/AnimationSettingsDialog.java
@@ -43,12 +43,7 @@ public class AnimationSettingsDialog {
 		JPanel contentPane = new JPanel(new GridBagLayout());
 		
 		JButton closeDialogButton = new JButton("Close");
-		closeDialogButton.addActionListener(new ActionListener() {
-			
-			public void actionPerformed(ActionEvent arg0) {
-				dialog.setVisible(false);
-			}
-		});
+		closeDialogButton.addActionListener(o -> dialog.setVisible(false));
 		
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.anchor = GridBagConstraints.NORTHWEST;
