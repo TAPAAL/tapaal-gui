8d3b924ca2a67a77d744aef4def78cbe5cf2bc06
Kenneth Yrke Jørgensen <kenneth@yrke.dk>
2020-07-03 09:25:41 +0200

    fixed typo

--- src/pipe/gui/widgets/ArcTokenSelector.java
+++ src/pipe/gui/widgets/ArcTokenSelector.java
@@ -66,7 +66,7 @@ public class ArcTokenSelector extends JPanel{
 	}
 	
 	private void updateInformationLabel(){
-		informationLabel.setText("Remaining to select" + missingToSelect + " tokens");
+		informationLabel.setText("Remaining to select " + missingToSelect + " tokens");
 	}
 	
 	public List<TimedToken> getSelected(){
