8a46e66d9f6086a4ced6474790e90edf3b5b5fb8
lsaid <lena.said@hotmail.com>
2020-07-08 12:58:21 +0200

    Changed the export information so it matches the engine.

--- src/dk/aau/cs/verification/VerifyTAPN/VerifyTAPNExporter.java
+++ src/dk/aau/cs/verification/VerifyTAPN/VerifyTAPNExporter.java
@@ -98,7 +98,7 @@ public class VerifyTAPNExporter {
 	private void outputTransition(TimedTransition t, PrintStream modelStream) {
 		modelStream.append("<transition ");
 
-        modelStream.append("player=\"" + (t.isUncontrollable()? "2" : "1") + "\" ");
+        modelStream.append("player=\"" + (t.isUncontrollable()? "1" : "0") + "\" ");
         modelStream.append("id=\"" + t.name() + "\" ");
 		modelStream.append("name=\"" + t.name() + "\" ");
 		modelStream.append("urgent=\"" + (t.isUrgent()? "true":"false") + "\"");
