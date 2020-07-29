c33a976c3098f8f76a6398cc30b226852526c5c0
Peter Taankvist ptaankvist@gmail.com <Peter Taankvist ptaankvist@gmail.com>
2020-07-29 15:59:37 +0200

    Adding EngineSupportOptions file

---
+++ src/pipe/gui/EngineSupportOptions.java
@@ -0,0 +1,74 @@
+package pipe.gui;
+
+import java.util.ArrayList;
+
+public class EngineSupportOptions {
+    private String nameString;
+    private boolean supportFastestTrace;
+    private boolean supportDeadlockNetdegree2EForAG;
+    private boolean supportDeadlockWithInhib;
+    private boolean supportWeights;
+    private boolean supportInhibArcs;
+    private boolean supportUrgentTransitions;
+    private boolean supportEGorAF;
+    private boolean supportStrictNets;
+    private boolean supportTimedNets;
+    private boolean[] optionsArray;
+    public EngineSupportOptions(String nameString, boolean supportFastestTrace, boolean supportDeadlockNetdegree2EForAG, boolean supportDeadlockEGorAF, boolean supportDeadlockWithInhib,
+                                boolean supportWeights, boolean supportInhibArcs, boolean supportUrgentTransitions, boolean supportEGorAF, boolean supportStrictNets, boolean supportTimedNets){
+        this.nameString = nameString;
+        this.supportFastestTrace =  supportFastestTrace;
+        this.supportDeadlockNetdegree2EForAG =  supportDeadlockNetdegree2EForAG;
+        this.supportDeadlockWithInhib =  supportDeadlockWithInhib;
+        this.supportWeights =  supportWeights;
+        this.supportInhibArcs =  supportInhibArcs;
+        this.supportUrgentTransitions =  supportUrgentTransitions;
+        this.supportEGorAF =  supportEGorAF;
+        this.supportStrictNets =  supportStrictNets;
+        this.supportTimedNets = supportTimedNets;
+        this.optionsArray = new boolean[]{supportFastestTrace, supportDeadlockNetdegree2EForAG, supportDeadlockEGorAF, supportDeadlockWithInhib, supportWeights, supportInhibArcs, supportUrgentTransitions, supportEGorAF, supportStrictNets, supportTimedNets};
+    }
+    public String getNameString(){
+        return nameString;
+    }
+    public boolean getSupportFastestTrace(){
+        return supportFastestTrace;
+    }
+    public boolean getSupportDeadlockNetdegree2EForAG(){
+        return supportDeadlockNetdegree2EForAG;
+    }
+    public boolean getSupportDeadlockWithInhib(){
+        return supportDeadlockWithInhib;
+    }
+    public boolean getSupportWeights(){
+        return supportWeights;
+    }
+    public boolean getSupportInhibArcs(){
+        return supportInhibArcs;
+    }
+    public boolean getSupportUrgentTransitions(){
+        return supportUrgentTransitions;
+    }
+    public boolean getSupportEGorAF(){
+        return supportEGorAF;
+    }
+    public boolean getSupportStrictNets(){
+        return supportStrictNets;
+    }
+    public boolean[] getOptionsAsArray(){
+        return optionsArray;
+    }
+    public boolean getSupportTimedNets(){
+        return supportTimedNets;
+    }
+
+    public boolean areOptionsSupported(boolean[] queryOptions){
+        for(int i = 0; i < optionsArray.length; i++){
+            if(queryOptions[i] == true && optionsArray[i] != true){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
