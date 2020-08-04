d9e7bb46c9b957181f6b571b057175cace8ed1a0
Kenneth Yrke Jørgensen <kenneth@yrke.dk>
2020-08-04 09:41:30 +0200

    Fixed wrong cast in TimedArcHandler for output arcs

--- src/pipe/gui/handler/TimedArcHandler.java
+++ src/pipe/gui/handler/TimedArcHandler.java
@@ -5,10 +5,12 @@ import java.awt.event.MouseEvent;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 
+import dk.aau.cs.model.tapn.TimedOutputArc;
 import pipe.gui.action.SplitArcAction;
 import pipe.gui.graphicElements.Arc;
 import pipe.gui.graphicElements.tapn.TimedInhibitorArcComponent;
 import pipe.gui.graphicElements.tapn.TimedInputArcComponent;
+import pipe.gui.graphicElements.tapn.TimedOutputArcComponent;
 import pipe.gui.graphicElements.tapn.TimedTransportArcComponent;
 
 public class TimedArcHandler extends ArcHandler {
@@ -24,7 +26,7 @@ public class TimedArcHandler extends ArcHandler {
 		JPopupMenu popup = super.getPopup(e);
 
 		menuItem = new JMenuItem("Properties");
-		menuItem.addActionListener(e1 -> ((TimedInputArcComponent) myObject).showTimeIntervalEditor());
+		menuItem.addActionListener(e1 -> ((TimedOutputArcComponent) myObject).showTimeIntervalEditor());
 		popup.insert(menuItem, popupIndex++);
 
 		popup.insert(new JPopupMenu.Separator(), popupIndex);
