491ef63ab5fe174d203543c00432442c82798526
lsaid <lena.said@hotmail.com>
2020-08-06 13:04:45 +0200

    Removed unnecessary check

--- src/dk/aau/cs/model/tapn/TimedArcPetriNet.java
+++ src/dk/aau/cs/model/tapn/TimedArcPetriNet.java
@@ -623,12 +623,6 @@ public class TimedArcPetriNet {
 		    return false;
         }
 
-		for(TransportArc t : transportArcs){
-		    if(!t.interval().equals(TimeInterval.ZERO_INF)){
-				return false;
-			}
-		}
-
 		if (hasUrgentTransitions()) {
 		    return false;
         }
