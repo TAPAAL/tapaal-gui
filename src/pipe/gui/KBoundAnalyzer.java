d116ef414a7376104314cda2e2796a64e1fbd746
lsaid <lena.said@hotmail.com>
2021-08-23 15:44:06 +0200

    Changed the boundedness check for untimed nets

--- src/pipe/gui/KBoundAnalyzer.java
+++ src/pipe/gui/KBoundAnalyzer.java
@@ -2,15 +2,18 @@ package pipe.gui;
 
 import javax.swing.JSpinner;
 
+import dk.aau.cs.TCTL.*;
+import dk.aau.cs.model.tapn.TimedArcPetriNet;
+import dk.aau.cs.model.tapn.TimedPlace;
+import dk.aau.cs.util.Tuple;
+import dk.aau.cs.verification.NameMapping;
+import dk.aau.cs.verification.TAPNComposer;
 import pipe.dataLayer.TAPNQuery.SearchOption;
 import pipe.dataLayer.TAPNQuery.TraceOption;
 import pipe.dataLayer.TAPNQuery.AlgorithmOption;
 import pipe.dataLayer.TAPNQuery.QueryCategory;
 import pipe.gui.widgets.RunningVerificationDialog;
 import dk.aau.cs.Messenger;
-import dk.aau.cs.TCTL.TCTLAGNode;
-import dk.aau.cs.TCTL.TCTLAbstractProperty;
-import dk.aau.cs.TCTL.TCTLTrueNode;
 import dk.aau.cs.model.tapn.TAPNQuery;
 import dk.aau.cs.model.tapn.TimedArcPetriNetNetwork;
 import dk.aau.cs.verification.ModelChecker;
@@ -21,6 +24,8 @@ import dk.aau.cs.verification.VerifyTAPN.VerifyPNOptions;
 import dk.aau.cs.verification.VerifyTAPN.VerifyTAPN;
 import dk.aau.cs.verification.VerifyTAPN.VerifyTAPNDiscreteVerification;
 import dk.aau.cs.verification.VerifyTAPN.VerifyTAPNOptions;
+
+import java.util.ArrayList;
 
 public class KBoundAnalyzer {
 	protected TimedArcPetriNetNetwork tapnNetwork;
@@ -40,7 +45,12 @@ public class KBoundAnalyzer {
 	}
 
 	public void analyze() {
-		TAPNQuery query = getBoundednessQuery();
+        TAPNQuery query;
+        if (modelChecker instanceof VerifyPN) {
+            query = getPNBoundednessQuery();
+        } else {
+            query = getBoundednessQuery();
+        }
 		VerifyTAPNOptions options = verificationOptions();
 
 		RunKBoundAnalysis analyzer = new RunKBoundAnalysis(modelChecker, messenger, spinner);
@@ -68,4 +78,38 @@ public class KBoundAnalyzer {
 
 		return new TAPNQuery(property, k);
 	}
+
+	protected TAPNQuery getPNBoundednessQuery() {
+        TCTLAbstractProperty property = null;
+        TimedArcPetriNet net = mergeNetComponents();
+        ArrayList<TCTLAbstractStateProperty> factors = new ArrayList<>();
+
+        tapnNetwork.sharedPlaces().forEach(o -> {
+            if (net.getPlaceByName(o.name()) != null && !net.getPlaceByName(o.name()).name().contains("Shared_")) {
+                net.getPlaceByName(o.name()).setName("Shared_" + o.name());
+            }
+        });
+        tapnNetwork.allTemplates().forEach(o -> o.places().forEach(x -> {
+            if (net.getPlaceByName(x.name()) != null) net.getPlaceByName(x.name()).setName(o.name() + "_" + x.name());
+        }));
+
+        for (TimedPlace place : net.places()) {
+            factors.add(new TCTLPlaceNode(place.name()));
+            factors.add(new AritmeticOperator("+"));
+        }
+        if (factors.get(factors.size()-1) instanceof AritmeticOperator) factors.remove(factors.size()-1);
+
+        TCTLAtomicPropositionNode child = new TCTLAtomicPropositionNode(new TCTLTermListNode(factors), ">", new TCTLConstNode(k));
+
+        property = new TCTLAGNode(child);
+
+        return new TAPNQuery(property, k);
+    }
+
+    private TimedArcPetriNet mergeNetComponents() {
+        TAPNComposer composer = new TAPNComposer(new MessengerImpl(), CreateGui.getCurrentTab().getGuiModels(), true, true);
+        Tuple<TimedArcPetriNet, NameMapping> transformedModel = composer.transformModel(tapnNetwork);
+
+        return transformedModel.value1();
+    }
 }
