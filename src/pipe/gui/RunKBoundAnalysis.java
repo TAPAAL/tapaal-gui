0ce053944af2bae46aeed4a93bd7e98d192c1f3a
lsaid <lena.said@hotmail.com>
2021-08-25 12:19:56 +0200

    updated bounded analysis check

--- src/pipe/gui/RunKBoundAnalysis.java
+++ src/pipe/gui/RunKBoundAnalysis.java
@@ -70,7 +70,7 @@ public class RunKBoundAnalysis extends RunVerificationBase {
 		return "The net with the specified extra number of tokens is bounded.\n\n"
 				+ "This means that the analysis will be exact and always give \n"
 				+ "the correct answer.\n\n"
-				+ "The number of extra tokens was automatically lowered to the\n"
-				+ "minimum number of tokens needed for an exact analysis.";
+				+ "The number of extra tokens can be lowered to the minimum \n"
+                + "number of tokens needed for an exact analysis.";
 	}
 }
