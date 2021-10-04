f100c7cfcf14a4e42753a1bcbc7e173726972893
lsaid <lena.said@hotmail.com>
2021-10-04 19:31:24 +0200

    Added shortcut to undo/redo in query dialog

--- src/pipe/gui/widgets/QueryDialog.java
+++ src/pipe/gui/widgets/QueryDialog.java
@@ -339,7 +339,8 @@ public class QueryDialog extends JPanel {
 		setLayout(new GridBagLayout());
 
 		init(option, queryToCreateFrom);
-		toggleAdvancedSimpleView(false);
+        makeShortcuts();
+        toggleAdvancedSimpleView(false);
 	}
 
 	private boolean checkIfSomeReductionOption() {
@@ -3347,5 +3348,25 @@ public class QueryDialog extends JPanel {
 		}
 
 	}
+
+    private void makeShortcuts(){
+        int shortcutkey = Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
+        ActionMap am = this.getActionMap();
+        am.put("undo", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                undoButton.doClick();
+            }
+        });
+        am.put("redo", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                redoButton.doClick();
+            }
+        });
+        InputMap im = this.getInputMap(JPanel.WHEN_IN_FOCUSED_WINDOW);
+        im.put(KeyStroke.getKeyStroke('Z', shortcutkey), "undo");
+        im.put(KeyStroke.getKeyStroke('Y', shortcutkey), "redo");
+    }
 
 }
