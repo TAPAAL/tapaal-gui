769baca3ae8fcab246af1c4529642cf0d7b24736
lsaid <lena.said@hotmail.com>
2022-08-01 09:14:58 +0200

    Saves file browser location in preferences

--- src/pipe/gui/widgets/filebrowser/FileBrowser.java
+++ src/pipe/gui/widgets/filebrowser/FileBrowser.java
@@ -5,12 +5,14 @@ import java.io.File;
 import dk.aau.cs.util.JavaUtil;
 import pipe.gui.CreateGui;
 
+import net.tapaal.Preferences;
+
 
 public abstract class FileBrowser  {
     //Used for latest open dialog path
     //Default value null makes the open dialog open default folder, For Windows, My Documents, For *nix  ~ , etc
 	//XXX 2018-05-23 moved from CreateGUI, refactor with regards to usage with lastPath local var in this class
-    public static String userPath = null;
+    public static String userPath = Preferences.getInstance().getFileBrowserLocation();
     static String lastSavePath = ".";
     static String lastOpenPath = ".";
 
@@ -22,18 +24,16 @@ public abstract class FileBrowser  {
 		return constructor(filetype, ext, "", path);
 	}
 	public static FileBrowser constructor(String filetype, final String ext, final String optionalExt, String path) {
+        Preferences.getInstance().setFileBrowserLocation(path);
 
-		if(JavaUtil.getJREMajorVersion() >= 7){
-
+		if (JavaUtil.getJREMajorVersion() >= 7) {
             return new NativeFileBrowser(filetype, ext, optionalExt, path);
-		}else{
+		} else {
 			/*if(path != null) {
 				newObject.lastPath = path;
 			}*/
 			return new NativeFileBrowserFallback(filetype, ext, optionalExt, path);
 		}
-
-
 	}
 
 	public abstract File openFile();
