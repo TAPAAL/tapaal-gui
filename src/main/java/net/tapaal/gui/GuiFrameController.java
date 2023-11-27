1314a0d4c31023cdd86d6697c7fe8cc2010a425c
mtygesen <mikkel.tygesen@gmail.com>
2023-11-27 18:45:03 +0100

    added missing error dialogue when program is not able to find a browser

--- src/main/java/net/tapaal/gui/GuiFrameController.java
+++ src/main/java/net/tapaal/gui/GuiFrameController.java
@@ -30,6 +30,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.lang.UnsupportedOperationException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -487,7 +488,7 @@ public final class GuiFrameController implements GuiFrameControllerActions{
         //open the default browser on this page
         try {
             java.awt.Desktop.getDesktop().browse(url);
-        } catch (IOException e) {
+        } catch (IOException | UnsupportedOperationException e) {
             Logger.log("Cannot open the browser.");
             e.printStackTrace();
             JOptionPane.showMessageDialog(null, "There was a problem opening the default web browser \n" +
