8843685f9350471f2391108d44f6128c7dbd3b78
Mikkel <mikkel.tygesen@gmail.com>
2024-02-11 16:28:41 +0100

    Fixed problem with id

--- src/main/java/dk/aau/cs/model/CPN/Variable.java
+++ src/main/java/dk/aau/cs/model/CPN/Variable.java
@@ -17,6 +17,9 @@ public class Variable {
     public void setName(String name){
         this.name = name;
     }
+
+    public void setId(String id) { this.id = id; }
+
     public String getName() {return this.name;}
 
     public String getId() {return this.id;}
