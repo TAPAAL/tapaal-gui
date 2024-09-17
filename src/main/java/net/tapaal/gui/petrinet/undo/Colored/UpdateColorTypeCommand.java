ec841c54909071b2ebd3416f99d796149ecd8a6a
TanguyDubois <dubois.tanguy@gmail.com>
2024-09-17 15:02:21 +0200

    Applied coding rules

--- src/main/java/net/tapaal/gui/petrinet/undo/Colored/UpdateColorTypeCommand.java
+++ src/main/java/net/tapaal/gui/petrinet/undo/Colored/UpdateColorTypeCommand.java
@@ -73,8 +73,9 @@ public class UpdateColorTypeCommand implements Command {
             for (TimedPlace place : tapn.places()) {
                 if (place.getColorType().equals(oldColorType)) {
 
-                    if(place.getTokensAsExpression() != null)
+                    if(place.getTokensAsExpression() != null) {
                         place.setTokenExpression(place.getTokensAsExpression().getExprConverted(oldColorType, newColorType));
+                    }
 
                     List<TimedToken> oldTokens = new ArrayList<>(place.tokens());
                     place.setColorType(newColorType);
