918d5712d874efa6470868dd4b6066c0433d6245
Mikkel Tygesen <mikkel.tygesen@gmail.com>
2024-11-18 17:09:45 +0100

    cleanup

--- src/main/java/dk/aau/cs/io/XmlUtil.java
+++ src/main/java/dk/aau/cs/io/XmlUtil.java
@@ -4,16 +4,20 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 public class XmlUtil {
+    /**
+     * Returns true if the given node is a descendant of a node with the given tag name.
+     */
     public static boolean isDescendantOfTag(Node node, String tagName) {
         Node parent = node.getParentNode();
         while (parent != null) {
-            if (parent.getNodeType() == Node.ELEMENT_NODE &&((Element) parent).getTagName().equals(tagName)) {
+            if (parent.getNodeType() == Node.ELEMENT_NODE && 
+                ((Element)parent).getTagName().equals(tagName)) {
                 return true;
             }
 
             parent = parent.getParentNode();
         }
-        
+
         return false;
     }
 }
