b1cdbe23094ecedd91b77198ebd0508a5d5fedb5
Mikkel Tygesen <mikkel.tygesen@gmail.com>
2025-01-04 02:24:10 +0100

    removed parentheses for add expressions

--- src/main/java/dk/aau/cs/model/CPN/Expressions/AddExpression.java
+++ src/main/java/dk/aau/cs/model/CPN/Expressions/AddExpression.java
@@ -189,11 +189,11 @@ public class AddExpression extends ArcExpression {
     @Override
     public String toString() {
 
-        StringBuilder res = new StringBuilder("(" + constituents.get(0).toString());
+        StringBuilder res = new StringBuilder(constituents.get(0).toString());
         for (int i = 1; i < constituents.size(); ++i) {
             res.append(" + ").append(constituents.get(i).toString());
         }
-        return res + ")";
+        return res;
     }
 
     public String toTokenString() {
