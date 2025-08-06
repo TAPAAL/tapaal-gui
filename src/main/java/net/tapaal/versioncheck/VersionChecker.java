10932e155266e338035d665b9769a115819bbb1e
Mikkel Tygesen <mikkel.tygesen@gmail.com>
2025-08-06 11:49:31 +0200

    version checking against prereleases

--- src/main/java/net/tapaal/versioncheck/VersionChecker.java
+++ src/main/java/net/tapaal/versioncheck/VersionChecker.java
@@ -64,13 +64,12 @@ public class VersionChecker {
 		int[] newestVersionNumbers = null;
 		try{
 			//currentVersionNumbers = getVersionNumbers(TAPAAL.VERSION);
-			currentVersionNumbers = getVersionNumbers(versionString);	
+            currentVersionNumbers = getVersionNumbers(versionString);
 			newestVersionNumbers = getVersionNumbers(newestVersion);
 		}catch(Exception e){
 			return false;
 		}
 		if(currentVersionNumbers.length != 3 || newestVersionNumbers.length != 3) return false;
-		
 		if(newestVersionNumbers[0] > currentVersionNumbers[0]){
 			return true;
 		}else if(newestVersionNumbers[0] == currentVersionNumbers[0] && newestVersionNumbers[1] > currentVersionNumbers[1]){
@@ -79,10 +78,14 @@ public class VersionChecker {
 			return true;
 		}
 		
-		return false;
+        // If current is pre-release newest version is newer
+        boolean isCurrentPreRelease = versionString.contains(" ");
+		return isCurrentPreRelease;
 	}
 
 	private int[] getVersionNumbers(String version) {
+        version = version.split("\\s+")[0]; // Remove pre-release suffix
+
 		String delimiter = "\\.";
 		String[] split = version.split(delimiter);
 		return new int[]{ Integer.parseInt(split[0]), Integer.parseInt(split[1]), Integer.parseInt(split[2])};
